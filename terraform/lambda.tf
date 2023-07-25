10385371342a21c76e64971b680d3b1a8a5f3068
Angus <angushirst@outlook.com>
2023-07-25 12:41:13 +0100

    update names so all read extract_data_to_ingestion_s3 for simplicity

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,14 +1,14 @@
 resource "aws_lambda_function" "extract_data_to_ingestion_s3" {
-  filename = "${path.module}/../extract_data_to_ingestion_s3.zip"
+  filename = "${path.module}/../function.zip"
   function_name = "extract_data_to_ingestion_s3"
   role = aws_iam_role.lambda_role.arn
   handler = "../src/extract/extract.py"
   runtime = "python3.9"
 }
 
-resource "aws_lambda_permission" "allow_extract_data_to_ingestion" {
+resource "aws_lambda_permission" "allow_extract_data_to_ingestion_s3" {
   action = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.extract_data_to_ingestion.function_name
+  function_name = aws_lambda_function.extract_data_to_ingestion_s3.function_name
   principal = "s3.amazonaws.com"
   source_arn = aws_s3_bucket.ingestion_zone.arn
   source_account = data.aws_caller_identity.current.account_id
