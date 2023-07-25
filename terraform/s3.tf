59196730b2c407e87d2813a20ec38c7f8214eef5
dabruge <davidbrucegeddes@gmail.com>
2023-07-25 16:04:58 +0100

    Remove s3 object creation in ingestion zone when terraform applied

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -5,10 +5,3 @@ resource "aws_s3_bucket" "ingestion_zone" {
 resource "aws_s3_bucket" "processed_zone" {
   bucket = "processed-va-052023"
 }
-
-resource "aws_s3_object" "lambda_code" {
-  bucket = aws_s3_bucket.ingestion_zone.bucket
-  key = "extract_data_to_ingestion_s3/function.zip"
-  source = "${path.module}/../function.zip"
-}
-
