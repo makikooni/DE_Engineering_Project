e421527959094bc76a58c66c4323b34bb64514f7
dabruge <davidbrucegeddes@gmail.com>
2023-07-25 15:53:37 +0100

    Remove redundant lambda and permissions

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -5,11 +5,3 @@ resource "aws_lambda_function" "extract_data_to_ingestion_s3" {
   handler = "extract.extraction_lambda_handler"
   runtime = "python3.9"
 }
-
-# resource "aws_lambda_permission" "allow_extract_data_to_ingestion_s3" {
-#   action = "lambda:InvokeFunction"
-#   function_name = aws_lambda_function.extract_data_to_ingestion_s3.function_name
-#   principal = "s3.amazonaws.com"
-#   source_arn = aws_s3_bucket.ingestion_zone.arn
-#   source_account = data.aws_caller_identity.current.account_id
-# }
--- terraform/s3.tf
+++ terraform/s3.tf
@@ -12,13 +12,3 @@ resource "aws_s3_object" "lambda_code" {
   source = "${path.module}/../function.zip"
 }
 
-resource "aws_s3_bucket_notification" "bucket_notification" {
-  bucket = aws_s3_bucket.ingestion_zone.id
-
-  lambda_function {
-    lambda_function_arn = aws_lambda_function.extract_data_to_ingestion_s3.arn
-    events              = ["s3:ObjectCreated:*"]
-  }
-
-  depends_on = [aws_lambda_permission.allow_extract_data_to_ingestion_s3]
-}
