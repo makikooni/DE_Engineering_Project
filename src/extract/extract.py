68c14095d47c0ad8efa423fa4523616ac141c1a5
Angus <angushirst@outlook.com>
2023-07-25 14:28:59 +0100

    add import os

--- src/extract/extract.py
+++ src/extract/extract.py
@@ -1,7 +1,8 @@
+import os
 '''
 Defines lambda function responsible for extracting the data 
 from the database and depositing it in the ingestion bucket
 '''
-def extraction_lambda_handler():
+def extraction_lambda_handler(event, context):
     print("Hello World!")
     pass
