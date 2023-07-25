0c41988db35c5f3be691d0994428d9c1c5fc9646
dabruge <davidbrucegeddes@gmail.com>
2023-07-25 14:58:05 +0100

    Add Hello World log functionality

--- src/extract/extract.py
+++ src/extract/extract.py
@@ -1,8 +1,17 @@
-import os
 '''
 Defines lambda function responsible for extracting the data 
 from the database and depositing it in the ingestion bucket
 '''
+
+import os
+import logging
+import boto3
+
+logger = logging.getLogger('MyLogger')
+logger.setLevel(logging.INFO)
+
 def extraction_lambda_handler(event, context):
-    print("Hello World!")
-    pass
+    logger.info(f'Hello World!')
+    return {
+        'statusCode': 200
+    }
