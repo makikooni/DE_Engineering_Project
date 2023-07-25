62741392bbe5d52ca96c48ea588a30a5041d5b63
Angus <angushirst@outlook.com>
2023-07-25 12:39:02 +0100

    set extract to simple print Hello World! function

--- src/extract/extract.py
+++ src/extract/extract.py
@@ -3,4 +3,5 @@ Defines lambda function responsible for extracting the data
 from the database and depositing it in the ingestion bucket
 '''
 def extraction_lambda_handler():
+    print("Hello World!")
     pass
