e6a34c537dd7f4d73128881a6d06188f7d4a8da3
tricia-mcmillan <sam.ms.adams132@gmail.com>
2023-07-26 16:35:09 +0100

    added secret manager to tests

--- exreacting_data_test/tests/test_extract_lambda_function.py
+++ exreacting_data_test/tests/test_extract_lambda_function.py
@@ -7,7 +7,7 @@ from moto import (
     )
 from pprint import pprint
 
-
+secret_string = '{"username":"project_user_3","password":"I4NX4jLv8i9VdeeM43uWBKPV","engine":"postgres","host":"nc-data-eng-totesys-production.chpsczt8h1nu.eu-west-2.rds.amazonaws.com","port":"5432","dbname":"totesys"}'
 def test_testing_function_imported_correctly():
     assert callable(put_table)
 
@@ -16,6 +16,7 @@ def test_returns_error_when_passed_invalid_table_name():
         put_table('123dhdhdh')
 
 @mock_s3
+@mock_secretsmanager
 def test_adds_object_to_s3_bucket():
     # create the secret
     conn = boto3.client('s3')
@@ -24,30 +25,39 @@ def test_adds_object_to_s3_bucket():
         CreateBucketConfiguration={
         'LocationConstraint': 'eu-west-2',
     })
-    put_table(tableName='design', bucketName='test_bucket')
+    
+    boto3.client("secretsmanager").create_secret(
+        Name="test_secret",
+        SecretString= secret_string 
+        )
+    put_table(tableName='design',bucketName='test_bucket',secretName='test_secret')
     obj = conn.list_objects_v2(Bucket='test_bucket')
-    pprint(obj)
-    assert obj['Name'] == 'test_bucke'
+    assert obj['Contents'][0]['Key']== 'design'
 
-# @mock_s3
-# def test_for_incorrect_bucket_name():
-#     conn = boto3.client('s3')
-#     # with pytest.raises(Exception) as e:
-#     err = put_table(tableName='design', bucketName='test_bucket')
-#     print(err)
+@mock_s3
+@mock_secretsmanager
+def test_for_incorrect_bucket_name():
+    conn = boto3.client('s3')
+    # to break the test
+    # conn.create_bucket(
+    #     Bucket='no_bucket',
+    #     CreateBucketConfiguration={
+    #     'LocationConstraint': 'eu-west-2',
+    # })
+    boto3.client("secretsmanager").create_secret(
+        Name="test_secret",
+        SecretString= secret_string 
+        )
+    with pytest.raises(Exception) as e:
+        put_table(tableName='design', bucketName='no_bucket', secretName='test_secret')
+    print(e)
+    assert 'Not a valid bucket' in str(e.value) 
 
-# @mock_secretsmanager
-# def test_for():
-#     # make secret with bad credentials
-#     client = boto3.client('secretsmanager')
-#     secrets = client.list_secrets()
-#     pprint(secrets)
-#     # client.create_secret(
-#     #     ClientRequestToken = f'${uuid.uuid1()}',
-#     #     Description='My secret',
-#     #     Name=f'{secret_identifier}',
-#     #     SecretString=secret_string
-#     # )
-#     # try to push new object to the bucket
-#     # spy on the console for connection to db not established
-#     assert 0
+@mock_secretsmanager
+def test_for_bad_secret_credentials():
+    # make secret with bad credentials
+    boto3.client("secretsmanager").create_secret(
+        Name="test_secret",
+        SecretString= '{"username":"project_user","password":"I4NX4jLv8i9VdeeM4","engine":"posts","host":"nc-data-eng-totesys-production.chp8h1nu.eu-west-2.rds.amazonaws.com","port":"5323","dbname":"tosys"}'
+        )
+    
