13978f8c868bcc76acb9edb9658e547e8d11d76a
dabruge <davidbrucegeddes@gmail.com>
2023-07-26 16:26:47 +0100

    Add sns alerting when NameError triggered

--- terraform/alarm.tf
+++ terraform/alarm.tf
@@ -25,3 +25,57 @@ resource "aws_cloudwatch_event_target" "lambda_target" {
   rule      = aws_cloudwatch_event_rule.scheduler.name
   arn       = aws_lambda_function.extract_data_to_ingestion_s3.arn
 }
+
+
+
+resource "aws_cloudwatch_log_metric_filter" "info_trigger" {
+    name = "Info"
+    pattern = "INFO"
+    log_group_name = "/aws/lambda/${aws_lambda_function.extract_data_to_ingestion_s3.function_name}"
+
+    metric_transformation {
+        name = "InfoSum"
+        namespace = "InfoLambdaMetrics"
+        value = "1"
+    }
+}
+resource "aws_cloudwatch_metric_alarm" "alert_info" {
+    alarm_name = "test-info"
+    metric_name = "InfoSum"
+    namespace = "InfoLambdaMetrics"
+    alarm_description = "Info has been logged in the log group"
+    comparison_operator = "GreaterThanOrEqualToThreshold"
+    statistic = "Sum"
+    evaluation_periods = 1
+    period = 60
+    threshold = 2
+    alarm_actions = [aws_sns_topic.extract_errors.arn]
+    ok_actions = [aws_sns_topic.extract_errors.arn]
+}
+
+
+
+resource "aws_cloudwatch_log_metric_filter" "name_error" {
+    name = "NameErrorFilter"
+    pattern = "NameError"
+    log_group_name = "/aws/lambda/${aws_lambda_function.extract_data_to_ingestion_s3.function_name}"
+
+    metric_transformation {
+        name = "NameErrorSum"
+        namespace = "NameErrorLambdaMetrics"
+        value = "1"
+    }
+}
+resource "aws_cloudwatch_metric_alarm" "alert_name_error" {
+    alarm_name = "test-name-error"
+    metric_name = "NameErrorSum"
+    namespace = "NameErrorLambdaMetrics"
+    alarm_description = "A NameError has occurred."
+    comparison_operator = "GreaterThanOrEqualToThreshold"
+    statistic = "Sum"
+    evaluation_periods = 1
+    period = 60
+    threshold = 2
+    alarm_actions = [aws_sns_topic.extract_errors.arn]
+    ok_actions = [aws_sns_topic.extract_errors.arn]
+}
