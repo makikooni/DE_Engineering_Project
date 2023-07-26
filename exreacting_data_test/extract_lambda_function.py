033c5780770aeef391db7b232635a4931f0fa159
LouKelly <louiskelly1999@gmail.com>
2023-07-26 11:02:14 +0100

    testing update

--- exreacting_data_test/extract_lambda_function.py
+++ exreacting_data_test/extract_lambda_function.py
@@ -1,7 +1,80 @@
 import pg8000
 import boto3
 import pandas as pd
+from pprint import pprint
+from botocore.exceptions import ClientError
 
+def put_table(tableName, bucketName='test-va-0423'):
+    session = boto3.Session(
+    aws_access_key_id='AKIAWT3PTZCGJVV2RIGV',
+    aws_secret_access_key='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'
+    )
+    s3 = session.resource('s3')
+    host = 'nc-data-eng-totesys-production.chpsczt8h1nu.eu-west-2.rds.amazonaws.com'
+    port = '5432'
+    database = 'totesys'
+    user = 'project_user_3'
+    password = 'I4NX4jLv8i9VdeeM43uWBKPV'
+    try:
+        connection = pg8000.connect(
+            host=host,
+            port=port,
+            database=database,
+            user=user,
+            password=password
+        )
+        cursor = connection.cursor()
+        sqlColumns = "select * from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME='"+tableName+"';"
+        cursor.execute(sqlColumns) # gets the column names
+        columnsRaw = cursor.fetchall()# gets the column names
+        columns = []# gets the column names
+        for columnName in columnsRaw:
+            columns.append(columnName[3])# gets the column names
+        sqlTable = "SELECT * FROM "+tableName+";"
+        cursor.execute(sqlTable) # gets the data from a table (in this case design)
+        rows = []
+        rows.append(columns)
+        rows.append(cursor.fetchall())# adds the column names to the start of the data
+        df = pd.DataFrame(data=rows[1], columns=rows[0]) # uses pandas to convert to a dataframe
+        output = df.to_string()
+        fileName = tableName + ".csv"
+        obj = s3.Object(bucketName, fileName)# adds the data to the s3 (this case test-va-0423), as a csv file (test_design.csv)
+        obj.put(Body=output)# adds the data to the s3 (this case test-va-0423), as a csv file (test_design.csv)
+        cursor.close()
+        connection.close()
+        print("Connection closed.")
+        
+    except pg8000.Error as e:
+        raise NameError(f"Error connecting to the database: {e}")
+        # print(f"Error connecting to the database: {e}")
+    
+    except ClientError as e:
+       if e.response['Error']['Code'] == 'NoSuchBucket':
+           raise Exception('Not a valid bucket')
+        
+        # print(e.value())
+        # pass
+
+        # print(dir(e))
+
+put_table(tableName='design', bucketName='test-va-0424')
+
+# def get_table():
+#     session = boto3.Session(
+#     aws_access_key_id='AKIAWT3PTZCGJVV2RIGV',
+#     aws_secret_access_key='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'
+#     )
+
+#     client = boto3.client('s3')
+#     response = client.get_object(
+#         Bucket='test-va-0423',
+#         Key='test_design.csv'
+#     )
+#     print(response)
+
+#     bucket = client._bucket('test-va-0423')
+#     for obj in bucket.get_all_keys():
+#         print(obj.key)
 
 # def put_table(tableName):
 #     session = boto3.Session(
@@ -48,65 +121,7 @@ import pandas as pd
 #         print(f"Error connecting to the database: {e}")
 
 
-def put_table(tableName, bucketName='test-va-0423'):
-    session = boto3.Session(
-    aws_access_key_id='AKIAWT3PTZCGJVV2RIGV',
-    aws_secret_access_key='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'
-    )
-    s3 = session.resource('s3')
-    host = 'nc-data-eng-totesys-production.chpsczt8h1nu.eu-west-2.rds.amazonaws.com'
-    port = '5432'
-    database = 'totesys'
-    user = 'project_user_3'
-    password = 'I4NX4jLv8i9VdeeM43uWBKPV'
-    try:
-        connection = pg8000.connect(
-            host=host,
-            port=port,
-            database=database,
-            user=user,
-            password=password
-        )
-        cursor = connection.cursor()
-        sqlColumns = "select * from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME='"+tableName+"';"
-        cursor.execute(sqlColumns) # gets the column names
-        columnsRaw = cursor.fetchall()# gets the column names
-        columns = []# gets the column names
-        for columnName in columnsRaw:
-            columns.append(columnName[3])# gets the column names
-        sqlTable = "SELECT * FROM "+tableName+";"
-        cursor.execute(sqlTable) # gets the data from a table (in this case design)
-        rows = []
-        rows.append(columns)
-        rows.append(cursor.fetchall())# adds the column names to the start of the data
-        df = pd.DataFrame(data=rows[1], columns=rows[0]) # uses pandas to convert to a dataframe
-        output = df.to_string()
-        fileName = "test_" + tableName + ".csv"
-        obj = s3.Object(bucketName, fileName)# adds the data to the s3 (this case test-va-0423), as a csv file (test_design.csv)
-        obj.put(Body=output)# adds the data to the s3 (this case test-va-0423), as a csv file (test_design.csv)
-        cursor.close()
-        connection.close()
-        print("Connection closed.")
-        
-    except pg8000.Error as e:
-        print(f"Error connecting to the database: {e}")
 
-# def get_table():
-#     session = boto3.Session(
-#     aws_access_key_id='AKIAWT3PTZCGJVV2RIGV',
-#     aws_secret_access_key='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'
-#     )
-
-#     client = boto3.client('s3')
-#     response = client.get_object(
-#         Bucket='test-va-0423',
-#         Key='test_design.csv'
-#     )
-#     print(response)
-
-#     bucket = client._bucket('test-va-0423')
-#     for obj in bucket.get_all_keys():
-#         print(obj.key)
 
 
     # s3 = session.resource('s3')
@@ -147,7 +162,7 @@ def put_table(tableName, bucketName='test-va-0423'):
 
 # put_table('counterparty')
 # put_table('sales_order')
-put_table('design')
+
 
 # put_table('address')
 
--- exreacting_data_test/tests/test_extract_lambda_function.py
+++ exreacting_data_test/tests/test_extract_lambda_function.py
@@ -1,19 +1,32 @@
 from exreacting_data_test.extract_lambda_function import put_table
-# import moto3
+import pytest
+import boto3
+from moto import mock_s3
+from pprint import pprint
 
 def test_testing_function_imported_correctly():
     assert callable(put_table)
 
-def test_returns_error_when_passed_invalid_table():
-    try:
-        put_table('design')
-        # fix this test
-        assert False
-    except:
-        assert True
+def test_returns_error_when_passed_invalid_table_name():
+    with pytest.raises(Exception) as e:
+        put_table('123dhdhdh')
 
+@mock_s3
 def test_adds_object_to_s3_bucket():
-    pass
+    conn = boto3.client('s3')
+    conn.create_bucket(
+        Bucket='test_bucket',
+        CreateBucketConfiguration={
+        'LocationConstraint': 'eu-west-2',
+    })
+    put_table(tableName='design', bucketName='test_bucket')
+    obj = conn.list_objects_v2(Bucket='test_bucket')
+    assert obj['Name'] == 'test_bucket'
 
-def test_():
-    pass
+@mock_s3
+def test_for_incorrect_bucket_name():
+    conn = boto3.client('s3')
+    
+    # with pytest.raises(Exception) as e:
+    err = put_table(tableName='design', bucketName='test_bucket')
+    print(err)
