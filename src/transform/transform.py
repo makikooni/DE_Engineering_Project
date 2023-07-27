32146bb9ca54e227961ca9bb6c7335529d16fb58
Angus <angushirst@outlook.com>
2023-07-27 21:49:16 +0100

    dim_location, dim_transaction added

--- src/transform/transform.py
+++ src/transform/transform.py
@@ -31,7 +31,27 @@ def transformation_lambda_handler():
         payment_type_table = pd.read_csv(f's3://{ingestion_bucket_name}/payment_type.csv')
         dim_payment_type_table = payment_type_table[['payment_type_id', 'payment_type_name']]
         dim_payment_type_table.to_parquet(f's3://{processing_bucket_name}/test_dim_payment_type.parquet')
-        # run in terminal to view pq table --> parquet-tools show s3://processed-va-052023/test_dim_payment_type.parque
+        # run in terminal to view pq table --> parquet-tools show s3://processed-va-052023/test_dim_payment_type.parquet
+
+        # dim_location table
+        address_table = pd.read_csv(f's3://{ingestion_bucket_name}/address.csv')
+        dim_address_table = address_table[['address_id', 'address_line_1', 'address_line_2', 'district', 'city', 'postal_code', 'country', 'phone']]
+        dim_address_table.rename(columns={'address_id': 'location_id'}, inplace=True)
+        dim_address_table.to_parquet(f's3://{processing_bucket_name}/test_dim_location.parquet')
+
+        # dim_transaction table
+        transaction_table = pd.read_csv(f's3://{ingestion_bucket_name}/transaction.csv')
+        dim_transaction_table = transaction_table[['transaction_id', 'transaction_type', 'sales_order_id', 'purchase_order_id']]
+        dim_transaction_table.to_parquet(f's3://{processing_bucket_name}/test_dim_transaction.parquet')
+
+        # dim_staff table
+        staff_table = pd.read_csv(f's3://{ingestion_bucket_name}/staff.csv')
+        department_table = pd.read_csv(f's3://{ingestion_bucket_name}/department.csv')
+        pprint(staff_table)
+        pprint(department_table)
+
+        # dim_transaction_table = transaction_table[['transaction_id', 'transaction_type', 'sales_order_id', 'purchase_order_id']]
+        # dim_transaction_table.to_parquet(f's3://{processing_bucket_name}/test_dim_transaction.parquet')
         
 
 
