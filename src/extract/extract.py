a3ccc643c51dde02c77f0a1701084ab39481f925
tricia-mcmillan <sam.ms.adams132@gmail.com>
2023-07-27 15:14:28 +0100

    put extraction function into autopep

--- src/extract/extract.py
+++ src/extract/extract.py
@@ -1,5 +1,4 @@
 import json
-import os
 import logging
 from pprint import pprint
 import pg8000
@@ -15,37 +14,39 @@ from the database and depositing it in the ingestion bucket
 logger = logging.getLogger('MyLogger')
 logger.setLevel(logging.INFO)
 
+
 def extraction_lambda_handler(event, context):
 
     extraction_lambda_function(tableName='design')
-    logger.info(f'Hello World!')
     return {
         'statusCode': 200
     }
 
-def extraction_lambda_function(tableName, bucketName='test-va-0423', secretName='ingestion/db'):
+
+def extraction_lambda_function(
+        tableName, bucketName='test-va-0423', secretName='ingestion/db'):
     secretsmanager = boto3.client('secretsmanager')
 
-    try: 
+    try:
         db_credentials = secretsmanager.get_secret_value(
-        SecretId = secretName
+            SecretId=secretName
         )
     except ClientError as _e:
         if _e.response['Error']['Code'] == 'ResourceNotFoundException':
             print("in error")
-            raise Exception(f"The requested secret was not found")
+            raise Exception("The requested secret was not found")
         else:
             print(_e)
-    db_creds = json.loads(db_credentials['SecretString']) # gets database credentials from secrets manager
+    # gets database credentials from secrets manager
+    db_creds = json.loads(db_credentials['SecretString'])
     s3 = boto3.resource('s3')
     host = db_creds['host']
-    port = db_creds['port'] 
-    # port = '0808'     
-    
+    port = db_creds['port']
+    # port = '0808'
+
     database = db_creds['dbname']
     user = db_creds['username']
     password = db_creds['password']
-
 
     try:
         connection = pg8000.connect(
@@ -56,40 +57,41 @@ def extraction_lambda_function(tableName, bucketName='test-va-0423', secretName=
             password=password
         )
         cursor = connection.cursor()
-        sqlColumns = "select * from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME='"+tableName+"';"
-        cursor.execute(sqlColumns) # gets the column names
-        columnsRaw = cursor.fetchall()# gets the column names
-        columns = []# gets the column names
+        sqlColumns = "select * from "\
+            "INFORMATION_SCHEMA.COLUMNS" \
+            " where TABLE_NAME='" + tableName + "';"
+        cursor.execute(sqlColumns)  # gets the column names
+        columnsRaw = cursor.fetchall()  # gets the column names
+        columns = []  # gets the column names
         for columnName in columnsRaw:
-            columns.append(columnName[3])# gets the column names
-        sqlTable = "SELECT * FROM "+tableName+";"
-        cursor.execute(sqlTable) # gets the data from a table (in this case design)
+            columns.append(columnName[3])  # gets the column names
+        sqlTable = "SELECT * FROM " + tableName + ";"
+        # gets the data from a table (in this case design)
+        cursor.execute(sqlTable)
         rows = []
         rows.append(columns)
-        rows.append(cursor.fetchall())# adds the column names to the start of the data
-        df = pd.DataFrame(data=rows[1], columns=rows[0]) # uses pandas to convert to a dataframe
+        # adds the column names to the start of the data
+        rows.append(cursor.fetchall())
+        # uses pandas to convert to a dataframe
+        df = pd.DataFrame(data=rows[1], columns=rows[0])
         output = df.to_csv()
         fileName = tableName
-        obj = s3.Object(bucketName, fileName)# adds the data to the s3 (this case test-va-0423), as a csv file (test_design.csv)
-        obj.put(Body=str(output))# adds the data to the s3 (this case test-va-0423), as a csv file (test_design.csv)
+        # adds the data to the s3 (this case test-va-0423), as a csv file
+        # (test_design.csv)
+        obj = s3.Object(bucketName, fileName)
+        # adds the data to the s3 (this case test-va-0423), as a csv file
+        # (test_design.csv)
+        obj.put(Body=str(output))
         cursor.close()
         connection.close()
         print("Connection closed.")
-        
+
     except pg8000.Error as _e:
         raise NameError(f"Error connecting to the database: {_e}")
-    
+
     except ClientError as _e:
         pprint(_e.response)
         if _e.response['Error']['Code'] == 'NoSuchBucket':
             raise Exception('Not a valid bucket')
         else:
             print(_e)
-
-
-    logger.info(f'Hello World!')
-    raise NameError
-    return {
-        'statusCode': 200
-    }
-
