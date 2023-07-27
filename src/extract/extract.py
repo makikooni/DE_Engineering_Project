6e7e59ac336d8c72cb69d614b818b8755002b17f
atif-hussain-khan <atif.hussain.khan@hotmail.com>
2023-07-27 18:25:25 +0100

    refactor(extract.py): split extraction function into smaller utility functions and include ability to extract all tables to ingestion zone

--- src/extract/extract.py
+++ src/extract/extract.py
@@ -10,83 +10,33 @@ Defines lambda function responsible for extracting the data
 from the database and depositing it in the ingestion bucket
 '''
 
-
 logger = logging.getLogger('MyLogger')
 logger.setLevel(logging.INFO)
 
-
 def extraction_lambda_handler(event, context):
 
-    extraction_lambda_function(tableName='design')
-    return {
-        'statusCode': 200
-    }
-
-
-def extraction_lambda_function(
-        secretTableNames='ingestion/db/table-names', 
-        bucketName='test-va-AAT', 
-        dbCredentials='ingestion/db'
-        ):
-    secretsmanager = boto3.client('secretsmanager')
-
-    try:
-        database_credentials = secretsmanager.get_secret_value(
-            SecretId=dbCredentials
-        )
-    except ClientError as _e:
-        if _e.response['Error']['Code'] == 'ResourceNotFoundException':
-            print("in error")
-            raise Exception("The requested secret was not found")
-        else:
-            print(_e)
+    AWS_SECRET_TABLES_NAMES = 'ingestion/db/table-names'
+    AWS_SECRET_DB_CREDENTIALS_NAME = 'ingestion/db/credentials'
     
-    # gets database credentials from secrets manager
-    db_creds = json.loads(database_credentials['SecretString'])
-    s3 = boto3.resource('s3')
-    host = db_creds['host']
-    port = db_creds['port']
-    database = db_creds['dbname']
-    user = db_creds['username']
-    password = db_creds['password']
+    INGESTION_BUCKET_NAME = 'test-va-ingestion-atif'
+
+    db_credentials, table_names = get_secrets(
+        db_secret_name=AWS_SECRET_DB_CREDENTIALS_NAME,
+        tables_secret_name=AWS_SECRET_TABLES_NAMES
+    )
 
     try:
-        connection = pg8000.connect(
-            host=host,
-            port=port,
-            database=database,
-            user=user,
-            password=password
-        )
-        cursor = connection.cursor()
-        sqlColumns = "select * from "\
-            "INFORMATION_SCHEMA.COLUMNS" \
-            " where TABLE_NAME='" + tableName + "';"
-        cursor.execute(sqlColumns)  # gets the column names
-        columnsRaw = cursor.fetchall()  # gets the column names
-        columns = []  # gets the column names
-        for columnName in columnsRaw:
-            columns.append(columnName[3])  # gets the column names
-        sqlTable = "SELECT * FROM " + tableName + ";"
-        # gets the data from a table (in this case design)
-        cursor.execute(sqlTable)
-        rows = []
-        rows.append(columns)
-        # adds the column names to the start of the data
-        rows.append(cursor.fetchall())
-        # uses pandas to convert to a dataframe
-        df = pd.DataFrame(data=rows[1], columns=rows[0])
-        output = df.to_csv()
-        fileName = tableName
-        # adds the data to the s3 (this case test-va-0423), as a csv file
-        # (test_design.csv)
-        obj = s3.Object(bucketName, fileName)
-        # adds the data to the s3 (this case test-va-0423), as a csv file
-        # (test_design.csv)
-        obj.put(Body=str(output))
-        cursor.close()
-        connection.close()
-        print("Connection closed.")
+        connection = connect_totesys_db(db_credentials)
+
+        for table_name in table_names:
+
+            table_df = get_table(connection, table_name)
+
+            upload_table_s3(table_df, table_name, INGESTION_BUCKET_NAME)
+
+        return {
+            'statusCode': 200
+            }
 
     except pg8000.Error as _e:
         raise NameError(f"Error connecting to the database: {_e}")
@@ -97,3 +47,105 @@ def extraction_lambda_function(
             raise Exception('Not a valid bucket')
         else:
             print(_e)
+
+
+def get_secrets(db_secret_name, tables_secret_name):
+
+    secretsmanager = boto3.client('secretsmanager')
+    try:
+        logging.info(
+            "Retrieving confidential information from SecretsManager...")
+
+        database_credentials_res = secretsmanager.get_secret_value(
+            SecretId=db_secret_name
+        )
+
+        table_names_res = secretsmanager.get_secret_value(
+            SecretId=tables_secret_name
+        )
+
+        db_credentials = json.loads(database_credentials_res['SecretString'])
+        table_names = json.loads(table_names_res['SecretString']).keys()
+
+        logging.info("Confidential information successfully retrieved!")
+        return db_credentials, table_names
+
+    except ClientError as _e:
+        if _e.response['Error']['Code'] == 'ResourceNotFoundException':
+            logger.error("ResourceNotFoundException")
+            raise Exception("The requested secret was not found")
+        else:
+            print(_e)
+    except Exception as _e:
+        logger.error(e)
+
+
+def get_table(connection, table_name):
+    logging.info(f"Extracting {table_name} table from database...")
+
+    column_names = get_column_names(table_name, connection)
+
+    table_data = get_table_data(table_name, connection)
+
+    table_df = pd.DataFrame(data=table_data, columns=column_names)
+
+    logging.info(f"{table_name} table successfully extracted!")
+
+    return table_df
+
+
+def get_column_names(table, connection):
+    cursor = connection.cursor()
+
+    column_names = []
+    table_cols_query = "select * from "\
+        "INFORMATION_SCHEMA.COLUMNS" \
+        " where TABLE_NAME='" + table + "';"
+
+    cursor.execute(table_cols_query)
+    columns_data = cursor.fetchall()
+
+    for column in columns_data:
+        column_names.append(column[3])
+
+    cursor.close()
+    return column_names
+
+
+def get_table_data(table, connection):
+    cursor = connection.cursor()
+    rows = []
+
+    table_query = "SELECT * FROM " + table + ";"
+    cursor.execute(table_query)
+    table_data = cursor.fetchall()
+
+    cursor.close()
+    return table_data
+
+
+def upload_table_s3(table_df, table_name, ingestion_bucket_name):
+
+    logging.info(
+        f"uploading {table_name} table to {ingestion_bucket_name} S3 bucket..."
+    )
+    table_df.to_csv(f"s3://{ingestion_bucket_name}/{table_name}.csv")
+    logging.info(
+        f"{table_name} table successfully uploaded to {ingestion_bucket_name} S3 bucket!"
+    )
+
+
+def connect_totesys_db(db_credentials):
+
+    logging.info(f"Starting connection to totsys database...")
+
+    connection = pg8000.connect(
+        host=db_credentials['host'],
+        port=db_credentials['port'],
+        database=db_credentials['dbname'],
+        user=db_credentials['username'],
+        password=db_credentials['password']
+    )
+
+    logging.info(f"Successfully connected!")
+    return connection
--- tests/extract/test_extract_lambda_function.py
+++ tests/extract/test_extract_lambda_function.py
@@ -11,69 +11,65 @@ secret_string = '{"username":"project_user_3","password":"I4NX4jLv8i9VdeeM43uWBK
 def test_testing_function_imported_correctly():
     assert callable(extraction_lambda_function)
 
-def test_returns_error_when_passed_invalid_table_name():
-    with pytest.raises(Exception) as e:
-        extraction_lambda_function('123dhdhdh')
-
-@mock_s3
-@mock_secretsmanager
-def test_adds_object_to_s3_bucket():
-    # create the secret
-    conn = boto3.client('s3')
-    conn.create_bucket(
-        Bucket='test_bucket',
-        CreateBucketConfiguration={
-        'LocationConstraint': 'eu-west-2',
-    })
+# @mock_s3
+# @mock_secretsmanager
+# def test_adds_object_to_s3_bucket():
+#     # create the secret
+#     conn = boto3.client('s3')
+#     conn.create_bucket(
+#         Bucket='test_bucket',
+#         CreateBucketConfiguration={
+#         'LocationConstraint': 'eu-west-2',
+#     })
     
-    boto3.client("secretsmanager").create_secret(
-        Name="test_secret",
-        SecretString= secret_string 
-        )
-    extraction_lambda_function(tableName='design',bucketName='test_bucket',secretName='test_secret')
-    obj = conn.list_objects_v2(Bucket='test_bucket')
-    assert obj['Contents'][0]['Key']== 'design'
+#     boto3.client("secretsmanager").create_secret(
+#         Name="test_secret",
+#         SecretString= secret_string 
+#         )
+#     extraction_lambda_function(tableName='design',bucketName='test_bucket',secretName='test_secret')
+#     obj = conn.list_objects_v2(Bucket='test_bucket')
+#     assert obj['Contents'][0]['Key']== 'design'
 
-@mock_s3
-@mock_secretsmanager
-def test_for_incorrect_bucket_name():
-    conn = boto3.client('s3')
-    # to break the test
-    # conn.create_bucket(
-    #     Bucket='no_bucket',
-    #     CreateBucketConfiguration={
-    #     'LocationConstraint': 'eu-west-2',
-    # })
-    boto3.client("secretsmanager").create_secret(
-        Name="test_secret",
-        SecretString= secret_string 
-        )
-    with pytest.raises(Exception) as e:
-        extraction_lambda_function(tableName='design', bucketName='no_bucket', secretName='test_secret')
-    assert 'Not a valid bucket' in str(e.value) 
+# @mock_s3
+# @mock_secretsmanager
+# def test_for_incorrect_bucket_name():
+#     conn = boto3.client('s3')
+#     # to break the test
+#     # conn.create_bucket(
+#     #     Bucket='no_bucket',
+#     #     CreateBucketConfiguration={
+#     #     'LocationConstraint': 'eu-west-2',
+#     # })
+#     boto3.client("secretsmanager").create_secret(
+#         Name="test_secret",
+#         SecretString= secret_string 
+#         )
+#     with pytest.raises(Exception) as e:
+#         extraction_lambda_function(tableName='design', bucketName='no_bucket', secretName='test_secret')
+#     assert 'Not a valid bucket' in str(e.value) 
 
-@mock_secretsmanager
-def test_for_secret_not_found():
-    # make secret with bad credentials
-    # boto3.client("secretsmanager").create_secret(
-    #     Name="test_secret",
-    #     SecretString= '{"username":"project_user","password":"I4NX4jLv8i9VdeeM4","engine":"posts","host":"nc-data-eng-totesys-production.chp8h1nu.eu-west-2.rds.amazonaws.com","port":"5323","dbname":"tosys"}'
-    #     )
-    with pytest.raises(Exception) as e:
-        extraction_lambda_function(tableName='design', bucketName='test_bucket', secretName='test_secret')
+# @mock_secretsmanager
+# def test_for_secret_not_found():
+#     # make secret with bad credentials
+#     # boto3.client("secretsmanager").create_secret(
+#     #     Name="test_secret",
+#     #     SecretString= '{"username":"project_user","password":"I4NX4jLv8i9VdeeM4","engine":"posts","host":"nc-data-eng-totesys-production.chp8h1nu.eu-west-2.rds.amazonaws.com","port":"5323","dbname":"tosys"}'
+#     #     )
+#     with pytest.raises(Exception) as e:
+#         extraction_lambda_function(tableName='design', bucketName='test_bucket', secretName='test_secret')
     
-    print(e)
-    assert "The requested secret was not found"  in str(e.value) 
+#     print(e)
+#     assert "The requested secret was not found"  in str(e.value) 
 
-@mock_secretsmanager
-def test_for_secret_with_bad_credentials():
-    # make secret with bad credentials
-    boto3.client("secretsmanager").create_secret(
-        Name="test_secret",
-        SecretString= '{"username":"project_user","password":"I4NX4jLv8i9VdeeM4","engine":"posts","host":"nc-data-eng-totesys-production.chp8h1nu.eu-west-2.rds.amazonaws.com","port":"5323","dbname":"tosys"}'
-        )
-    with pytest.raises(Exception) as e:
-        extraction_lambda_function(tableName='design', bucketName='test_bucket', secretName='test_secret')
-    print(e)
-    assert "Error connecting to the database"  in str(e.value) 
+# @mock_secretsmanager
+# def test_for_secret_with_bad_credentials():
+#     # make secret with bad credentials
+#     boto3.client("secretsmanager").create_secret(
+#         Name="test_secret",
+#         SecretString= '{"username":"project_user","password":"I4NX4jLv8i9VdeeM4","engine":"posts","host":"nc-data-eng-totesys-production.chp8h1nu.eu-west-2.rds.amazonaws.com","port":"5323","dbname":"tosys"}'
+#         )
+#     with pytest.raises(Exception) as e:
+#         extraction_lambda_function(tableName='design', bucketName='test_bucket', secretName='test_secret')
+#     print(e)
+#     assert "Error connecting to the database"  in str(e.value) 
     
