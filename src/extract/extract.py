07fc2c94c0e5daa2122e588006551ba2f0d203e8
atif-hussain-khan <atif.hussain.khan@hotmail.com>
2023-07-28 17:42:48 +0100

    test(get_secret): add tests for get_secret utility function

--- src/extract/extract.py
+++ src/extract/extract.py
@@ -1,83 +1,73 @@
 import json
 import logging
-from pprint import pprint
 import pg8000
 import boto3
 import pandas as pd
 from botocore.exceptions import ClientError
-'''
+
+"""
 Defines lambda function responsible for extracting the data
 from the database and depositing it in the ingestion bucket
-'''
+"""
 
-logger = logging.getLogger('MyLogger')
+logger = logging.getLogger("MyLogger")
 logger.setLevel(logging.INFO)
 
+
 def extraction_lambda_handler(event, context):
+    AWS_SECRET_TABLES_NAMES = "ingestion/db/table-names"
+    AWS_SECRET_DB_CREDENTIALS_NAME = "ingestion/db/credentials"
 
-    AWS_SECRET_TABLES_NAMES = 'ingestion/db/table-names'
-    AWS_SECRET_DB_CREDENTIALS_NAME = 'ingestion/db/credentials'
-    
-    INGESTION_BUCKET_NAME = 'test-va-ingestion-atif'
+    INGESTION_BUCKET_NAME = "test-va-ingestion-atif"
 
-    db_credentials, table_names = get_secrets(
-        db_secret_name=AWS_SECRET_DB_CREDENTIALS_NAME,
-        tables_secret_name=AWS_SECRET_TABLES_NAMES
-    )
+    db_credentials_response = get_secret(AWS_SECRET_DB_CREDENTIALS_NAME)
+    table_names_response = get_secret(AWS_SECRET_TABLES_NAMES)
+
+    db_credentials = json.loads(db_credentials_response["SecretString"])
+    table_names = json.loads(table_names_response["SecretString"]).keys()
 
     try:
         connection = connect_totesys_db(db_credentials)
 
         for table_name in table_names:
-
             table_df = get_table(connection, table_name)
 
             upload_table_s3(table_df, table_name, INGESTION_BUCKET_NAME)
 
-        return {
-            'statusCode': 200
-            }
+        return {"statusCode": 200}
 
     except pg8000.Error as _e:
         raise NameError(f"Error connecting to the database: {_e}")
 
     except ClientError as _e:
-        pprint(_e.response)
-        if _e.response['Error']['Code'] == 'NoSuchBucket':
-            raise Exception('Not a valid bucket')
+        logging.error(_e.response)
+        if _e.response["Error"]["Code"] == "NoSuchBucket":
+            raise Exception("Not a valid bucket")
         else:
-            print(_e)
+            logging.error(_e)
 
 
-def get_secrets(db_secret_name, tables_secret_name):
-
-    secretsmanager = boto3.client('secretsmanager')
+def get_secret(secret_name):
+    secretsmanager = boto3.client("secretsmanager")
     try:
-        logging.info(
-            "Retrieving confidential information from SecretsManager...")
+        logging.info(f"Retrieving {secret_name} information from SecretsManager...")
 
-        database_credentials_res = secretsmanager.get_secret_value(
-            SecretId=db_secret_name
+        response = secretsmanager.get_secret_value(
+            SecretId=secret_name
         )
 
-        table_names_res = secretsmanager.get_secret_value(
-            SecretId=tables_secret_name
-        )
-
-        db_credentials = json.loads(database_credentials_res['SecretString'])
-        table_names = json.loads(table_names_res['SecretString']).keys()
-
-        logging.info("Confidential information successfully retrieved!")
-        return db_credentials, table_names
-
-    except ClientError as _e:
-        if _e.response['Error']['Code'] == 'ResourceNotFoundException':
-            logger.error("ResourceNotFoundException")
-            raise Exception("The requested secret was not found")
+        logging.info(f"{secret_name} information successfully retrieved!")
+        
+        return response
+    
+    except ClientError as e:
+        if e.response["Error"]["Code"] == "ResourceNotFoundException":
+            logging.error(f"ResourceNotFoundException: the secret named {secret_name} cannot be found in SecretsManager")
+            raise KeyError(f"ResourceNotFoundException: the secret named {secret_name} cannot be found in SecretsManager")
         else:
-            print(_e)
-    except Exception as _e:
-        logger.error(e)
+            logging.error(e)
+    except Exception as e:
+        logging.error(e)
 
 
 def get_table(connection, table_name):
@@ -98,9 +88,11 @@ def get_column_names(table, connection):
     cursor = connection.cursor()
 
     column_names = []
-    table_cols_query = "select * from "\
-        "INFORMATION_SCHEMA.COLUMNS" \
+    table_cols_query = (
+        "select * from "
+        "INFORMATION_SCHEMA.COLUMNS"
         " where TABLE_NAME='" + table + "';"
+    )
 
     cursor.execute(table_cols_query)
     columns_data = cursor.fetchall()
@@ -125,7 +117,6 @@ def get_table_data(table, connection):
 
 
 def upload_table_s3(table_df, table_name, ingestion_bucket_name):
-
     logging.info(
         f"uploading {table_name} table to {ingestion_bucket_name} S3 bucket..."
     )
@@ -136,15 +127,14 @@ def upload_table_s3(table_df, table_name, ingestion_bucket_name):
 
 
 def connect_totesys_db(db_credentials):
-
     logging.info(f"Starting connection to totsys database...")
 
     connection = pg8000.connect(
-        host=db_credentials['host'],
-        port=db_credentials['port'],
-        database=db_credentials['dbname'],
-        user=db_credentials['username'],
-        password=db_credentials['password']
+        host=db_credentials["host"],
+        port=db_credentials["port"],
+        database=db_credentials["dbname"],
+        user=db_credentials["username"],
+        password=db_credentials["password"],
     )
 
     logging.info(f"Successfully connected!")
