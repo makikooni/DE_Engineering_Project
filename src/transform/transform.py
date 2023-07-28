330993aa7fa4cf3c8ad103a3c53638e39b0b9a1b
Angus <angushirst@outlook.com>
2023-07-28 16:47:53 +0100

    dim_dates in progress. end of day commit

--- src/transform/transform.py
+++ src/transform/transform.py
@@ -10,10 +10,13 @@ import numpy as np
 logger = logging.getLogger('MyLogger')
 logger.setLevel(logging.INFO)
 
+ingestion_bucket_name = 'test-va-ingestion-atif'
+processing_bucket_name = 'processed-va-052023'
+
+date_col_dataframes = []
+
 def transformation_lambda_handler():
     # pd.set_option('display.max_columns', None)
-    ingestion_bucket_name = 'test-va-ingestion-atif'
-    processing_bucket_name = 'processed-va-052023'
     try:
         s3_client = boto3.client('s3')
         s3_resource = boto3.resource('s3')
@@ -22,17 +25,20 @@ def transformation_lambda_handler():
         if status_code != 200:
             raise Exception('the bucket may not exist, or, you may not have the correct permissions')
         
+
         # dim_design table
         design_table = pd.read_csv(f's3://{ingestion_bucket_name}/design.csv')
         dim_design_table = design_table[['design_id', 'design_name', 'file_location', 'file_name']]
         dim_design_table.to_parquet(f's3://{processing_bucket_name}/test_dim_design.parquet')
         # run in terminal to view pq table --> parquet-tools show s3://processed-va-052023/test_dim_design.parquet
 
+
         # dim_payment_type table
         payment_type_table = pd.read_csv(f's3://{ingestion_bucket_name}/payment_type.csv')
         dim_payment_type_table = payment_type_table[['payment_type_id', 'payment_type_name']]
         dim_payment_type_table.to_parquet(f's3://{processing_bucket_name}/test_dim_payment_type.parquet')
         # run in terminal to view pq table --> parquet-tools show s3://processed-va-052023/test_dim_payment_type.parquet
+
 
         # dim_location table
         address_table = pd.read_csv(f's3://{ingestion_bucket_name}/address.csv')
@@ -41,11 +47,13 @@ def transformation_lambda_handler():
         dim_address_table.to_parquet(f's3://{processing_bucket_name}/test_dim_location.parquet')
         # run in terminal to view pq table --> parquet-tools show s3://processed-va-052023/test_dim_location.parquet
 
+
         # dim_transaction table
         transaction_table = pd.read_csv(f's3://{ingestion_bucket_name}/transaction.csv')
         dim_transaction_table = transaction_table[['transaction_id', 'transaction_type', 'sales_order_id', 'purchase_order_id']]
         dim_transaction_table.to_parquet(f's3://{processing_bucket_name}/test_dim_transaction.parquet')
         # run in terminal to view pq table --> parquet-tools show s3://processed-va-052023/test_dim_transaction.parquet
+
 
         # dim_staff table
         staff_table = pd.read_csv(f's3://{ingestion_bucket_name}/staff.csv')
@@ -55,6 +63,7 @@ def transformation_lambda_handler():
         dim_staff_table.to_parquet(f's3://{processing_bucket_name}/test_dim_staff.parquet')
         # run in terminal to view pq table --> parquet-tools show s3://processed-va-052023/test_dim_staff.parquet
 
+
         # dim_currency table
         currency_table = pd.read_csv(f's3://{ingestion_bucket_name}/currency.csv')
         dim_currency_table = currency_table[['currency_id', 'currency_code']]
@@ -63,6 +72,7 @@ def transformation_lambda_handler():
         dim_currency_table['currency_name'] = np.select(conditions, values)
         dim_currency_table.to_parquet(f's3://{processing_bucket_name}/test_dim_currency.parquet')
         # run in terminal to view pq table --> parquet-tools show s3://processed-va-052023/test_dim_currency.parquet
+
 
         # dim_counterparty table
         counterparty_table = pd.read_csv(f's3://{ingestion_bucket_name}/counterparty.csv')
@@ -75,65 +85,13 @@ def transformation_lambda_handler():
         dim_counterparty.to_parquet(f's3://{processing_bucket_name}/test_dim_counterparty.parquet')
         # run in terminal to view pq table --> parquet-tools show s3://processed-va-052023/test_dim_counterparty.parquet
 
-        # fact_sales_order table
-        sales_order_table = pd.read_csv(f's3://{ingestion_bucket_name}/sales_order.csv')
-        sales_order_table.columns.values[0] = "sales_record_id"
-        
-        new_created_sales = sales_order_table['created_at'].str.split(" ", n = 1, expand = True)
-        sales_order_table['created_date']= new_created_sales[0]
-        sales_order_table['created_time']= new_created_sales[1]
-        sales_order_table.drop(columns =['created_at'], inplace = True)
+        transform_sales_order()
+        transform_purchase_order()
+        transform_payment()
 
-        new_updated_sales = sales_order_table['last_updated'].str.split(" ", n = 1, expand = True)
-        sales_order_table['last_updated_date']= new_updated_sales[0]
-        sales_order_table['last_updated_time']= new_updated_sales[1]
-        sales_order_table.drop(columns =['last_updated'], inplace = True)
+        pprint(date_col_dataframes)
 
-        sales_order_table.rename(columns={'staff_id': 'sales_staff_id'}, inplace=True)
-        fact_sales_order = sales_order_table[['sales_record_id', 'sales_order_id', 'created_date', 'created_time', 'last_updated_date', 'last_updated_time', 'sales_staff_id', 'counterparty_id', 'units_sold', 'unit_price', 'currency_id', 'design_id', 'agreed_payment_date', 'agreed_delivery_date', 'agreed_delivery_location_id']]
-        fact_sales_order.to_parquet(f's3://{processing_bucket_name}/test_fact_sales_order.parquet')
-        # run in terminal to view pq table --> parquet-tools show s3://processed-va-052023/test_fact_sales_order.parquet
-
-        # fact_purchase_order table
-        purchase_order_table = pd.read_csv(f's3://{ingestion_bucket_name}/purchase_order.csv')
-        purchase_order_table.columns.values[0] = "purchase_record_id"
-
-        new_created_purchase = purchase_order_table['created_at'].str.split(" ", n = 1, expand = True)
-        purchase_order_table['created_date']= new_created_purchase[0]
-        purchase_order_table['created_time']= new_created_purchase[1]
-        purchase_order_table.drop(columns =['created_at'], inplace = True)
-
-        new_updated_purchase = purchase_order_table['last_updated'].str.split(" ", n = 1, expand = True)
-        purchase_order_table['last_updated_date']= new_updated_purchase[0]
-        purchase_order_table['last_updated_time']= new_updated_purchase[1]
-        purchase_order_table.drop(columns =['last_updated'], inplace = True)
-
-        fact_purchase_order = purchase_order_table[['purchase_record_id', 'purchase_order_id', 'created_date', 'created_time', 'last_updated_date', 'last_updated_time', 'staff_id', 'counterparty_id', 'item_code', 'item_quantity', 'item_unit_price', 'currency_id', 'agreed_delivery_date', 'agreed_payment_date', 'agreed_delivery_location_id']]
-        fact_purchase_order.to_parquet(f's3://{processing_bucket_name}/test_fact_purchase_order.parquet')
-        # run in terminal to view pq table --> parquet-tools show s3://processed-va-052023/test_fact_purchase_order.parquet
-
-        # fact_payment table
-        payment_table = pd.read_csv(f's3://{ingestion_bucket_name}/payment.csv')
-        payment_table.columns.values[0] = 'payment_record_id'
-
-        new_created_payment = payment_table['created_at'].str.split(" ", n = 1, expand = True)
-        payment_table['created_date']= new_created_payment[0]
-        payment_table['created_time']= new_created_payment[1]
-        payment_table.drop(columns =['created_at'], inplace = True)
-
-        new_updated_payment = payment_table['last_updated'].str.split(" ", n = 1, expand = True)
-        payment_table['last_updated_date']= new_updated_payment[0]
-        payment_table['last_updated_time']= new_updated_payment[1]
-        payment_table.drop(columns =['last_updated'], inplace = True)
-        
-        # figma states last_updated, assuming this is a typo: find last_updated_time
-        fact_payment_table = payment_table[['payment_record_id', 'payment_id', 'created_date', 'created_time', 'last_updated_date', 'last_updated_time', 'transaction_id', 'counterparty_id', 'payment_amount', 'currency_id', 'payment_type_id', 'paid', 'payment_date']]
-        fact_payment_table.to_parquet(f's3://{processing_bucket_name}/test_fact_payment.parquet')
-        # run in terminal to view pq table --> parquet-tools show s3://processed-va-052023/test_fact_payment.parquet
-
-
-
-
+        # dim_date table
 
 
 
@@ -145,8 +103,72 @@ def transformation_lambda_handler():
         pass
 
 
-transformation_lambda_handler()
 
+
+def transform_sales_order():
+    # fact_sales_order table
+    sales_order_table = pd.read_csv(f's3://{ingestion_bucket_name}/sales_order.csv')
+    sales_order_table.columns.values[0] = "sales_record_id"
+    
+    new_created_sales = sales_order_table['created_at'].str.split(" ", n = 1, expand = True)
+    sales_order_table['created_date']= new_created_sales[0]
+    sales_order_table['created_time']= new_created_sales[1]
+    sales_order_table.drop(columns =['created_at'], inplace = True)
+
+    new_updated_sales = sales_order_table['last_updated'].str.split(" ", n = 1, expand = True)
+    sales_order_table['last_updated_date']= new_updated_sales[0]
+    sales_order_table['last_updated_time']= new_updated_sales[1]
+    sales_order_table.drop(columns =['last_updated'], inplace = True)
+
+    sales_order_table.rename(columns={'staff_id': 'sales_staff_id'}, inplace=True)
+    fact_sales_order = sales_order_table[['sales_record_id', 'sales_order_id', 'created_date', 'created_time', 'last_updated_date', 'last_updated_time', 'sales_staff_id', 'counterparty_id', 'units_sold', 'unit_price', 'currency_id', 'design_id', 'agreed_payment_date', 'agreed_delivery_date', 'agreed_delivery_location_id']]
+    fact_sales_order.to_parquet(f's3://{processing_bucket_name}/test_fact_sales_order.parquet')
+    date_cols_to_add = [fact_sales_order['created_date'], fact_sales_order['last_updated_date'],fact_sales_order['agreed_payment_date'], fact_sales_order['agreed_delivery_date']]
+    for col in date_cols_to_add:
+        date_col_dataframes.append(col)
+    # run in terminal to view pq table --> parquet-tools show s3://processed-va-052023/test_fact_sales_order.parquet
+
+def transform_purchase_order():
+    # fact_purchase_order table
+    purchase_order_table = pd.read_csv(f's3://{ingestion_bucket_name}/purchase_order.csv')
+    purchase_order_table.columns.values[0] = "purchase_record_id"
+
+    new_created_purchase = purchase_order_table['created_at'].str.split(" ", n = 1, expand = True)
+    purchase_order_table['created_date']= new_created_purchase[0]
+    purchase_order_table['created_time']= new_created_purchase[1]
+    purchase_order_table.drop(columns =['created_at'], inplace = True)
+
+    new_updated_purchase = purchase_order_table['last_updated'].str.split(" ", n = 1, expand = True)
+    purchase_order_table['last_updated_date']= new_updated_purchase[0]
+    purchase_order_table['last_updated_time']= new_updated_purchase[1]
+    purchase_order_table.drop(columns =['last_updated'], inplace = True)
+
+    fact_purchase_order = purchase_order_table[['purchase_record_id', 'purchase_order_id', 'created_date', 'created_time', 'last_updated_date', 'last_updated_time', 'staff_id', 'counterparty_id', 'item_code', 'item_quantity', 'item_unit_price', 'currency_id', 'agreed_delivery_date', 'agreed_payment_date', 'agreed_delivery_location_id']]
+    fact_purchase_order.to_parquet(f's3://{processing_bucket_name}/test_fact_purchase_order.parquet')
+    # run in terminal to view pq table --> parquet-tools show s3://processed-va-052023/test_fact_purchase_order.parquet
+
+def transform_payment():
+    # fact_payment table
+    payment_table = pd.read_csv(f's3://{ingestion_bucket_name}/payment.csv')
+    payment_table.columns.values[0] = 'payment_record_id'
+
+    new_created_payment = payment_table['created_at'].str.split(" ", n = 1, expand = True)
+    payment_table['created_date']= new_created_payment[0]
+    payment_table['created_time']= new_created_payment[1]
+    payment_table.drop(columns =['created_at'], inplace = True)
+
+    new_updated_payment = payment_table['last_updated'].str.split(" ", n = 1, expand = True)
+    payment_table['last_updated_date']= new_updated_payment[0]
+    payment_table['last_updated_time']= new_updated_payment[1]
+    payment_table.drop(columns =['last_updated'], inplace = True)
+
+    # figma states last_updated, assuming this is a typo: find last_updated_time
+    fact_payment_table = payment_table[['payment_record_id', 'payment_id', 'created_date', 'created_time', 'last_updated_date', 'last_updated_time', 'transaction_id', 'counterparty_id', 'payment_amount', 'currency_id', 'payment_type_id', 'paid', 'payment_date']]
+    fact_payment_table.to_parquet(f's3://{processing_bucket_name}/test_fact_payment.parquet')
+    # run in terminal to view pq table --> parquet-tools show s3://processed-va-052023/test_fact_payment.parquet
+
+
+transformation_lambda_handler()
 
 
 '''
