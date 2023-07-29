14915bedea4f6d528f55831fa98dd8d89fc5a043
atif-hussain-khan <atif.hussain.khan@hotmail.com>
2023-07-29 11:38:11 +0100

    testing(get_table): define tests for get_ table function

---
+++ tests/extract/test_get_table.py
@@ -0,0 +1,25 @@
+from src.extract.extract import get_secret
+import pytest
+import boto3
+from unittest.mock import patch
+from moto import mock_secretsmanager
+
+
+def test_should_return_dataframe():
+    pass
+
+
+def test_dataframe_should_contain_correct_data():
+    pass
+
+
+def test_should_protect_against_sql_injection():
+    pass
+
+
+def test_should_raise_exception_if_incorrect_input_type():
+    pass
+
+
+def test_should_raise_exception_if_table_name_does_not_exist():
+    pass
