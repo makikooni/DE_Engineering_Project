4262864f1a25e239efccfe7eb6e87d7edc841665
atif-hussain-khan <atif.hussain.khan@hotmail.com>
2023-07-30 14:02:09 +0100

    apply code formatting

--- src/extract/extract.py
+++ src/extract/extract.py
@@ -27,7 +27,7 @@ def extraction_lambda_handler(event, context):
     table_names = json.loads(table_names_response["SecretString"]).keys()
 
     try:
-        connection = connect_db(db_credentials, db_name = "totesys")
+        connection = connect_db(db_credentials, db_name="totesys")
 
         for table_name in table_names:
             table_df, query = get_table(connection, table_name)
@@ -55,18 +55,20 @@ def get_secret(secret_name):
     try:
         logging.info(f"Retrieving {secret_name} information from SecretsManager...")
 
-        response = secretsmanager.get_secret_value(
-            SecretId=secret_name
-        )
+        response = secretsmanager.get_secret_value(SecretId=secret_name)
 
         logging.info(f"{secret_name} information successfully retrieved!")
-        
+
         return response
-    
+
     except ClientError as e:
         if e.response["Error"]["Code"] == "ResourceNotFoundException":
-            logging.error(f"ResourceNotFoundException: the secret named {secret_name} cannot be found in SecretsManager")
-            raise KeyError(f"ResourceNotFoundException: the secret named {secret_name} cannot be found in SecretsManager")
+            logging.error(
+                f"ResourceNotFoundException: the secret named {secret_name} cannot be found in SecretsManager"
+            )
+            raise KeyError(
+                f"ResourceNotFoundException: the secret named {secret_name} cannot be found in SecretsManager"
+            )
         else:
             logging.error(e)
     except Exception as e:
@@ -77,8 +79,9 @@ def get_table(connection, table_name):
     if not isinstance(table_name, str):
         raise TypeError(f"table name is {type(table_name)}, expected {str}")
     if not isinstance(connection, type(Connection)):
-        raise TypeError(f"connection object is {type(connection)}, expected {Connection}")
-
+        raise TypeError(
+            f"connection object is {type(connection)}, expected {Connection}"
+        )
 
     logging.info(f"Extracting {table_name} table from database...")
     try:
@@ -98,51 +101,54 @@ def get_table(connection, table_name):
 
 
 def upload_table_s3(table_df, table_name, ingestion_bucket_name):
-    
-    if not isinstance(table_df,pd.DataFrame):
+    if not isinstance(table_df, pd.DataFrame):
         raise TypeError(f"table dataframe {type(table_df)}, expected {pd.DataFrame}")
-    if not isinstance(table_name,str):
+    if not isinstance(table_name, str):
         raise TypeError(f"table name is {type(table_name)}, expected {str}")
-    if not isinstance(ingestion_bucket_name,str):
-        raise TypeError(f"ingestion bucket name is {type(ingestion_bucket_name)}, expected {str}")
+    if not isinstance(ingestion_bucket_name, str):
+        raise TypeError(
+            f"ingestion bucket name is {type(ingestion_bucket_name)}, expected {str}"
+        )
 
     try:
-        boto3.client('s3').head_bucket(Bucket=ingestion_bucket_name)
+        boto3.client("s3").head_bucket(Bucket=ingestion_bucket_name)
 
         logging.info(
             f"uploading {table_name} table to {ingestion_bucket_name} S3 bucket..."
         )
-                
+
         table_df.to_csv(f"s3://{ingestion_bucket_name}/{table_name}.csv")
-        
+
         logging.info(
             f"{table_name} table successfully uploaded to {ingestion_bucket_name} S3 bucket!"
         )
     except Exception as e:
-        error = e.response['Error']
-        if e.response['Error']['Code'] == 'NoSuchBucket':
+        error = e.response["Error"]
+        if e.response["Error"]["Code"] == "NoSuchBucket":
             logging.error(f"NoSuchBucket: {error['BucketName']} does not exist")
 
         raise KeyError(f"{error['BucketName']} does not exist")
 
 
-def connect_db(db_credentials, db_name = ""):
+def connect_db(db_credentials, db_name=""):
     logging.info(f"Performing checks before connecting to database...")
     if not isinstance(db_credentials, dict):
         raise TypeError(f"db_credentials is {type(db_credentials)}, {dict} is required")
-    elif not isinstance(db_name,str):
+    elif not isinstance(db_name, str):
         raise TypeError(f"db_name is {type(db_name)}, {str} is required")
-    
+
     db_credentials_keys = list(db_credentials.keys())
     required_keys = ["host", "port", "dbname", "username", "password"]
 
     for req_key in required_keys:
         if req_key not in db_credentials_keys:
-            raise KeyError(f'db_credentials contains does not contain {req_key}')
-    
+            raise KeyError(f"db_credentials contains does not contain {req_key}")
+
     for key in db_credentials:
         if not isinstance(db_credentials[key], str):
-            raise ValueError(f"value for {key} is {type(db_credentials[key])}, {str} is required")
+            raise ValueError(
+                f"value for {key} is {type(db_credentials[key])}, {str} is required"
+            )
 
     try:
         logging.info(f"Starting connection to {db_name} database...")
@@ -158,7 +164,7 @@ def connect_db(db_credentials, db_name = ""):
     except InterfaceError:
         logging.error(f"InterfaceError: please check your database credentials")
         raise InterfaceError
-    
+
     except DatabaseError:
         logging.error(f"DatabaseError: please contact your database administrator")
         raise DatabaseError
