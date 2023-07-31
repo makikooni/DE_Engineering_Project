0eb3b82996f375c1337dddd2ea8b862d0b7c67e3
Angus <angushirst@outlook.com>
2023-07-31 14:19:29 +0100

    add_to_dates_set function added

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -11,11 +11,14 @@ def timestamp_to_date_and_time(dataframe):
     dataframe['created_date']= new_created[0]
     dataframe['created_time']= new_created[1]
     dataframe.drop(columns =['created_at'], inplace = True)
-
     new_updated = dataframe['last_updated'].str.split(" ", n = 1, expand = True)
     dataframe['last_updated_date']= new_updated[0]
     dataframe['last_updated_time']= new_updated[1]
     dataframe.drop(columns =['last_updated'], inplace = True)
-
     return dataframe
+
+def add_to_dates_set(set, cols_to_add):
+    for col in cols_to_add:
+        for row in col:
+            set.add(row)
 
