fb612d4e9f7e2bfdde095967ff8f6500ad33f06d
atif-hussain-khan <atif.hussain.khan@hotmail.com>
2023-07-31 12:20:23 +0100

    test(extraction_lambda_handler): test the lambda function for invalid trigger schedule

--- src/extract/extract.py
+++ src/extract/extract.py
@@ -12,7 +12,7 @@ logger.setLevel(logging.INFO)
 
 def extraction_lambda_handler(event, context):
     """Handles scheduled invocation event and transfers data from database to s3.
-     
+
       One receipt of a Cloudwatch scheduled invocation event, connects to totesys database and
       extracts each table and uploads as a csv to ingestion zone (S3 bucket).
 
@@ -25,14 +25,26 @@ def extraction_lambda_handler(event, context):
             https://docs.aws.amazon.com/lambda/latest/dg/python-context.html
 
     Raises:
+        ValueError: Event resources arn does not match the expected arn.
         RuntimeError: An unexpected error occurred in execution. Other errors
         result in an informative log message.
     """
-    
-    s3_bucket_name, s3_object_name = get_object_path(event['Records'])
-    logger.info(f'Bucket is {s3_bucket_name}')
-    logger.info(f'Object key is {s3_object_name}')
-    
+    AWS_SECRET_TABLES_NAMES = "ingestion/db/table-names"
+    AWS_SECRET_DB_CREDENTIALS_NAME = "ingestion/db/credentials"
+
+    INGESTION_BUCKET_NAME = "test-va-ingestion-atif"
+    CLOUDWATCH_TRIGGER_ARN = (
+        "arn:aws:events:eu-west-2:454963742860:rule/extraction_schedule"
+    )
+
+    if event["resources"] != CLOUDWATCH_TRIGGER_ARN:
+        raise ValueError("Event schedule is incorrect")
+
+    logger.info(
+        f'The lambda has been triggered at {event["time"]} by a {event["detail-type"]} '
+    )
+    logger.info(f'event["detail"] is: \n {event["detail"]} ')
+
     AWS_SECRET_TABLES_NAMES = "ingestion/db/table-names"
     AWS_SECRET_DB_CREDENTIALS_NAME = "ingestion/db/credentials"
 
--- tests/extract/test_extract_lambda_function.py
+++ tests/extract/test_extract_lambda_function.py
@@ -1,7 +1,14 @@
 from src.extract.extract import extraction_lambda_handler
 import pytest
-import boto3
-from moto import (
-    mock_s3, 
-    )
+import json
 
+@pytest.fixture
+def invalid_event():
+    with open('tests/invalid_test_event.json') as v:
+        event = json.loads(v.read())
+    return event
+
+def test_should_not_raise_error_on_valid_event(invalid_event):
+    
+    with pytest.raises(ValueError):
+        extraction_lambda_handler(invalid_event, {})
