6e03284ef2e23229174b01627422640c7f5c9a91
atif-hussain-khan <atif.hussain.khan@hotmail.com>
2023-07-31 15:08:03 +0100

    as before

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,5 +1,5 @@
 resource "aws_lambda_function" "extract_data_to_ingestion_s3" {
-  filename = "${path.module}/../function.zip"
+  filename = "${path.module}/../extraction_function.zip"
   function_name = "extract_data_to_ingestion_s3"
   role = aws_iam_role.lambda_role.arn
   handler = "extract.extraction_lambda_handler"
