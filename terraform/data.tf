20bfabfa2e1a3f9eedf6b48d20ac6dad8c989e56
atif-hussain-khan <atif.hussain.khan@hotmail.com>
2023-07-31 15:06:59 +0100

    changed name of lambda zip resource to extraction_lambda_zip and extraction_functions.zip

--- terraform/data.tf
+++ terraform/data.tf
@@ -2,11 +2,11 @@ data "aws_caller_identity" "current" {}
 
 data "aws_region" "current" {}
 
-data "archive_file" "lambda" {
+data "archive_file" "extraction_lambda_zip" {
   type        = "zip"
   source_file = [
     "${path.module}/../src/extract/extract.py",
     "${path.module}/../src/utils"
     ]
-  output_path = "${path.module}/../function.zip"
+  output_path = "${path.module}/../extraction_function.zip"
 }
