297bbdd68a26339a4ad1598e9c5fd764769290e6
tricia-mcmillan <sam.ms.adams132@gmail.com>
2023-07-31 10:29:55 +0100

    fixed the sql problem and added placeholders

--- src/load/load.py
+++ src/load/load.py
@@ -4,7 +4,7 @@ import logging
 import boto3
 import pandas as pd
 from botocore.exceptions import ClientError
-def update_table(s3_table_name, wh_table_name, secret_name = 'warehouse'):
+def add_new_rows(s3_table_name, wh_table_name, secret_name = 'warehouse'):
     # get warehouse credentails from AWS secrets
     secretsmanager = boto3.client('secretsmanager')
     try:
@@ -12,17 +12,13 @@ def update_table(s3_table_name, wh_table_name, secret_name = 'warehouse'):
             SecretId=secret_name
         )
     except ClientError as error:
-        # if _e.response['Error']['Code'] == 'ResourceNotFoundException':
-            # raise Exception("The requested secret was not found")
-        raise error
-   
+        if error.response['Error']['Code'] == 'ResourceNotFoundException':
+            raise Exception("The requested secret was not found")
+        else:
+            raise error
     try:
-        
         # conecting to the process bucket
         process_bucket = 'processed-va-052023'
-        # s3_resource = boto3.resource('s3')   
-        # s3_client = boto3.client('s3')
-        # response = s3_client.head_bucket(Bucket=process_bucket)
         # credentails for the warehouse
         db_creds = json.loads(db_credentials['SecretString'])
         host = db_creds['host']
@@ -30,8 +26,6 @@ def update_table(s3_table_name, wh_table_name, secret_name = 'warehouse'):
         database = db_creds['dbname']
         user = db_creds['username']
         password = db_creds['password']
-        
-        
         # connecting to warehouse
         connection = pg8000.connect(
                     host=host,
@@ -42,34 +36,16 @@ def update_table(s3_table_name, wh_table_name, secret_name = 'warehouse'):
                 )
         # get the table from the s3 and put it in a pandas dataframe
         table = pd.read_parquet(f's3://{process_bucket}/{s3_table_name}')
-        table_data_frame = pd.DataFrame(table)
-        # build the sql string
+        columns = ', '.join(table.columns)
+        new_row = ',' .join(['%s'] * len(table.columns))
+        insert_table_sql = f"INSERT INTO {wh_table_name} ({columns}) VALUES ({new_row})"
+        # Convert DataFrame to list tuples for executemany
+        data_to_insert = [tuple(row) for row in table.itertuples(index=False)]
+        # Execute the query using executemany to insert all rows at once
         cursor = connection.cursor()
-        ## build the columns for the sql string
-        list_columns = ''
-        index_columns = 0
-        for column in list(table_data_frame.columns):
-            if index_columns != len(list(table_data_frame.columns))- 1:
-                list_columns += str(column) + ', '
-                index_columns += 1
-            else:
-                list_columns += column
-        print(list_columns)
-        ## build the new row for the sql string
-        new_row = ''
-        print(list(table_data_frame.loc[0]))
-        index_rows = 0
-        for row in list(list(table_data_frame.loc[0])):
-            if index_rows != len(list(table_data_frame.loc[0]))- 1:
-                new_row += str(row) + ', '
-                index_rows += 1
-            else:
-                new_row += row
-        print(new_row)
-        ## put the sql string together
-        update_table_sql = 'INSERT INTO ' + wh_table_name + ' (' +list_columns+') VALUES (' + new_row + ')' 
-        cursor.execute(update_table_sql)
+        cursor.executemany(insert_table_sql, data_to_insert)
+        connection.commit()
         cursor.close()
     except Exception as error:
         raise error
-update_table('test_dim_design.parquet', 'dim_design')
+add_new_rows('test_dim_design.parquet', 'dim_design')
