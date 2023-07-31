cc1b4703131f75a4c567224aab25be0bcfd16869
atif-hussain-khan <atif.hussain.khan@hotmail.com>
2023-07-31 15:04:00 +0100

    include /src/utils in function deployment package

--- terraform/data.tf
+++ terraform/data.tf
@@ -4,6 +4,9 @@ data "aws_region" "current" {}
 
 data "archive_file" "lambda" {
   type        = "zip"
-  source_file = "${path.module}/../src/extract/extract.py"
+  source_file = [
+    "${path.module}/../src/extract/extract.py",
+    "${path.module}/../src/utils"
+    ]
   output_path = "${path.module}/../function.zip"
 }
