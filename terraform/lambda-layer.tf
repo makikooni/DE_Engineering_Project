fb66040cae424b27d812cb3014c5faaa6bf3cfae
atif-hussain-khan <atif.hussain.khan@hotmail.com>
2023-07-31 16:15:45 +0100

    rename lambda-layer resources

--- terraform/lambda-layer.tf
+++ terraform/lambda-layer.tf
@@ -6,7 +6,7 @@ locals {
 }
 
 # create zip file from requirements.txt. Triggers only when the file is updated
-resource "null_resource" "lambda_layer" {
+resource "null_resource" "lambda_layer_zip" {
   triggers = {
     requirements = filesha1(local.requirements_path)
   }
@@ -16,7 +16,7 @@ resource "null_resource" "lambda_layer" {
       apt install zip -y
       rm -rf python
       mkdir python
-      pip3 install -r ${local.requirements_path} -t python/
+      pip install -r ${local.requirements_path} -t python/
       zip -r ${local.layer_zip_path} python/
     EOT
   }
@@ -27,19 +27,19 @@ resource "aws_s3_bucket" "lambda_layer_bucket" {
 }
 
 # upload zip file to s3
-resource "aws_s3_object" "lambda_layer_zip" {
+resource "aws_s3_object" "lambda_layer_zip_s3" {
   bucket     = aws_s3_bucket.lambda_layer_bucket.id
   key        = "lambda_layers/${local.layer_name}/${local.layer_zip_path}"
   source     = local.layer_zip_path
-  depends_on = [null_resource.lambda_layer] # triggered only if the zip file is created
+  depends_on = [null_resource.lambda_layer_zip] # triggered only if the zip file is created
 }
 
 # create lambda layer from s3 object
-resource "aws_lambda_layer_version" "my-lambda-layer" {
+resource "aws_lambda_layer_version" "lambda_layer" {
   s3_bucket           = aws_s3_bucket.lambda_layer_bucket.id
-  s3_key              = aws_s3_object.lambda_layer_zip.key
+  s3_key              = aws_s3_object.lambda_layer_zip_s3.key
   layer_name          = local.layer_name
   compatible_runtimes = ["python3.9"]
   skip_destroy        = true
-  depends_on          = [aws_s3_object.lambda_layer_zip] # triggered only if the zip file is uploaded to the bucket
+  depends_on          = [aws_s3_object.lambda_layer_zip_s3] # triggered only if the zip file is uploaded to the bucket
 }
