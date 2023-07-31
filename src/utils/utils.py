730be756c8dd1f8f82845accd5a3010c2adef3e9
Angus <angushirst@outlook.com>
2023-07-31 14:10:37 +0100

    timestamp_to_date_and_time function added

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -5,3 +5,17 @@ def read_csv_to_pandas(file, source_bucket):
 
 def write_df_to_parquet(df, file, target_bucket):
     return df.to_parquet(f's3://{target_bucket}/{file}.parquet')
+
+def timestamp_to_date_and_time(dataframe):
+    new_created = dataframe['created_at'].str.split(" ", n = 1, expand = True)
+    dataframe['created_date']= new_created[0]
+    dataframe['created_time']= new_created[1]
+    dataframe.drop(columns =['created_at'], inplace = True)
+
+    new_updated = dataframe['last_updated'].str.split(" ", n = 1, expand = True)
+    dataframe['last_updated_date']= new_updated[0]
+    dataframe['last_updated_time']= new_updated[1]
+    dataframe.drop(columns =['last_updated'], inplace = True)
+
+    return dataframe
+
