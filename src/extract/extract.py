d0c2e45f39030a547f898a6b422877b035dc1c4b
atif-hussain-khan <atif.hussain.khan@hotmail.com>
2023-07-31 10:26:28 +0100

    docs(extraction_lambda_handler): add documentation

--- src/extract/extract.py
+++ src/extract/extract.py
@@ -13,6 +13,24 @@ logger.setLevel(logging.INFO)
 
 
 def extraction_lambda_handler(event, context):
+    """Handles scheduled invocation event and transfers data from database to s3.
+     
+      One receipt of a Cloudwatch scheduled invocation event, connects to totesys database and
+      extracts each table and uploads as a csv to ingestion zone (S3 bucket).
+
+    Args:
+        event:
+            a valid Cloudwatch scheudle event -
+            see https://docs.aws.amazon.com/AmazonS3/latest/userguide/notification-content-structure.html
+        context:
+            a valid AWS lambda Python context object - see
+            https://docs.aws.amazon.com/lambda/latest/dg/python-context.html
+
+    Raises:
+        RuntimeError: An unexpected error occurred in execution. Other errors
+        result in an informative log message.
+    """
+
     AWS_SECRET_TABLES_NAMES = "ingestion/db/table-names"
     AWS_SECRET_DB_CREDENTIALS_NAME = "ingestion/db/credentials"
 
@@ -29,8 +47,6 @@ def extraction_lambda_handler(event, context):
 
             upload_table_s3(table_df, table_name, INGESTION_BUCKET_NAME)
 
-        return {"statusCode": 200}
-
     except Exception as e:
-        logging.error("some unidentified error has occured!")
-        raise e
+        logger.error(e)
+        raise RuntimeError
