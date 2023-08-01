c329501ee192c6c99ad029d821338d85091858d3
atif-hussain-khan <atif.hussain.khan@hotmail.com>
2023-08-01 16:35:13 +0100

    changed tests to mock connection type and attributes passed to get_table_db

--- tests/utils/test_get_table_db.py
+++ tests/utils/test_get_table_db.py
@@ -1,13 +1,13 @@
 from utils.utils import get_table_db
 import pytest
 import pandas as pd
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch, MagicMock, Mock
 from pg8000.native import Connection, InterfaceError
 
-@patch("utils.utils.Connection")
-def test_should_return_correct_dataframe_and_string(test_connection):
-    test_table_name = "test_table"
 
+@pytest.fixture
+def mock_connection():
+    test_connection = Mock(spec=Connection)
     test_connection.run.return_value = [
         ["r1c1", "r1c2", "r1c3"],
         ["r2c1", "r2c2", "r2c3"],
@@ -19,9 +19,13 @@ def test_should_return_correct_dataframe_and_string(test_connection):
         {"name": "col2"},
         {"name": "col3"},
     ]
+    yield test_connection
+
+def test_should_return_correct_dataframe_and_string(mock_connection):
+    test_connection = mock_connection
+    test_table_name = "test_table"
 
     test_df = pd.DataFrame(data=test_connection.run(), columns=["col1", "col2", "col3"])
-
     test_result_df, test_query = get_table_db(test_connection, test_table_name)
 
     assert isinstance(test_result_df, pd.DataFrame)
@@ -30,8 +34,8 @@ def test_should_return_correct_dataframe_and_string(test_connection):
     assert test_result_df.equals(test_df)
 
 
-@patch("utils.utils.Connection")
-def test_should_protect_against_sql_injection(test_connection):
+def test_should_protect_against_sql_injection(mock_connection):
+    test_connection = mock_connection
     test_table_name = "design; DROP *;"
     test_table_df, test_query = get_table_db(test_connection, test_table_name)
 
@@ -42,12 +46,12 @@ def test_should_raise_exception_if_incorrect_connection_input_type():
     with pytest.raises(TypeError):
         get_table_db([], "table_name")
 
-def test_should_raise_exception_if_incorrect_table_name_input_type():
+def test_should_raise_exception_if_incorrect_table_name_input_type(mock_connection):
     with pytest.raises(TypeError):
-        get_table_db(Connection, 2)
+        get_table_db(mock_connection, 2)
 
-@patch("utils.utils.Connection")
-def test_should_raise_error_for_invalid_table(test_connection):
+def test_should_raise_error_for_invalid_table(mock_connection):
+    test_connection = mock_connection
     test_table_name = "design; DROP *;"
     test_connection.run = MagicMock(side_effect=InterfaceError)
     
