92a99165d4819b2045a79fe8102cbea9a677758d
Angus <angushirst@outlook.com>
2023-08-01 08:14:31 +0100

    file added to mock s3 bucket

--- tests/transform/test_table_transformations.py
+++ tests/transform/test_table_transformations.py
@@ -10,17 +10,32 @@ import pytest
 @pytest.fixture(scope='function')
 def ingestion_s3():
     with mock_s3():
-        yield boto3.client('s3', region_name='eu-west-2')
+        '''
+        fixture creates 'test-ingestion-va-052023' bucket in 
+        mock aws account and uploads 'test.txt' file to it
+        '''
+        s3 = boto3.client('s3')
+        s3.create_bucket(Bucket='test-ingestion-va-052023', CreateBucketConfiguration={'LocationConstraint': 'us-west-1'})
+        with open('/Users/angushirst/Northcoders/week-11-project/teststuff/test.txt', 'rb') as data:
+            s3.upload_fileobj(data, 'test-ingestion-va-052023', 'test.txt')
+        yield s3
+
 
 @pytest.fixture(scope='function')
 def processed_s3():
     with mock_s3():
-        yield boto3.client('s3', region_name='eu-west-2')
+        '''
+        fixture creates 'test-processed-va-052023' bucket in mock aws account
+        '''
+        s3 = boto3.client('s3')
+        s3.create_bucket(Bucket='test-processed-va-052023', CreateBucketConfiguration={'LocationConstraint': 'us-west-1'})
+        yield s3
 
-
+# @mock_s3
 def test_transform_design_retrieves_file_from_ingestion_s3_bucket(ingestion_s3, processed_s3):
-
-    pass
+    pprint(ingestion_s3.list_objects(Bucket='test-ingestion-va-052023'))
+    # pprint(processed_s3.list_objects(Bucket='test-processed-va-052023'))
+    assert Falsegi
 
 
 def test_transform_design_transforms_tables_into_correct_parquet_shchema():
