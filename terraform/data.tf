7b3366cdb1100581ce7554ee7ae3a6e5c203dec5
atif-hussain-khan <atif.hussain.khan@hotmail.com>
2023-08-02 10:30:50 +0100

    fix bug in lambda deployment to include correct utils

--- terraform/data.tf
+++ terraform/data.tf
@@ -5,9 +5,16 @@ data "aws_region" "current" {}
 data "archive_file" "extraction_lambda_zip" {
   type        = "zip"
   excludes = [
+    "${path.module}/../src/.github",
+    "${path.module}/../src/tests",
+    "${path.module}/../src/.gitignore",
+    "${path.module}/../src/lambda_layer_requirements.txt",
+    "${path.module}/../src/Makefile",
+    "${path.module}/../src/README.md",
+    "${path.module}/../src/requirements.txt",
     "${path.module}/../src/load.py",
     "${path.module}/../src/transform.py"
   ]
-  source_dir = "${path.module}/../src"
+  source_dir = "${path.module}/.."
   output_path = "${path.module}/../extraction_function.zip"
 }
