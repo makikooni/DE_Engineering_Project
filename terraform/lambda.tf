9fbb6138b1815cc85a57dcb1773b66f94ce2a0cb
atif-hussain-khan <atif.hussain.khan@hotmail.com>
2023-08-02 15:35:39 +0100

    change lambda timeout to 1 minute

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -5,5 +5,6 @@ resource "aws_lambda_function" "extract_lambda" {
   handler = "extract.extraction_lambda_handler"
   runtime = "python3.9"
   layers = [aws_lambda_layer_version.lambda_layer.arn, "arn:aws:lambda:eu-west-2:336392948345:layer:AWSSDKPandas-Python39:8"]
+  timeout = 60
   depends_on = [null_resource.extraction_zip]
 }
