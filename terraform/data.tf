8fc5047d359a27fdadc46a5fd0397a352127854d
atif-hussain-khan <atif.hussain.khan@hotmail.com>
2023-08-02 12:16:42 +0100

    fix file path bug attempt 4

--- terraform/data.tf
+++ terraform/data.tf
@@ -3,13 +3,13 @@ data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 locals {
   extract_function_zip_path = "${path.module}/../extraction_function.zip"
-  extract_function_path = "${path.root}/../src/extract.py"
+  extract_function_path = "extract.py"
   utils_path = "${path.root}/../utils"
 }
 
 resource "null_resource" "extraction_zip" {
   provisioner "local-exec" {
-    command = "zip -r ${local.extract_function_zip_path} ${local.extract_function_path} ${local.utils_path}"
+    command = "cd ${path.root}/../src & zip -r ${local.extract_function_zip_path} ${local.extract_function_path} ${local.utils_path}"
   }
 }
 
