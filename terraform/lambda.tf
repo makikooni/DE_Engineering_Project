296f251dfbf5074cdcb67d52c878320df4e18812
atif-hussain-khan <atif.hussain.khan@hotmail.com>
2023-08-02 13:32:12 +0100

    add AWSSDKPandas-Python layer to extract lambda

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -4,6 +4,6 @@ resource "aws_lambda_function" "extract_lambda" {
   role = aws_iam_role.lambda_role.arn
   handler = "extract.extraction_lambda_handler"
   runtime = "python3.9"
-  layers = [aws_lambda_layer_version.lambda_layer.arn]
+  layers = [aws_lambda_layer_version.lambda_layer.arn, "arn:aws:lambda:eu-west-2:336392948345:layer:AWSSDKPandas-Python39:8"]
   depends_on = [null_resource.extraction_zip]
 }
