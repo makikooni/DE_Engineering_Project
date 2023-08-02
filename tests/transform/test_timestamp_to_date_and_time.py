4660702025d99b4fd5cd0d54e1ca962c9618e777
Angus <angushirst@outlook.com>
2023-08-02 21:51:21 +0100

    test timestamp_to_date_and_time correctly splits relative columns and formats relative data

---
+++ tests/transform/test_timestamp_to_date_and_time.py
@@ -0,0 +1,16 @@
+from moto import mock_s3
+from pandas.testing import assert_frame_equal
+import pandas as pd
+import pytest
+from pprint import pprint
+from src.utils.utils import timestamp_to_date_and_time
+
+
+def test_function_correctly_splits_columns_and_data():
+     
+    df = pd.read_csv('tests/transform/test_data_csv_files/test_timestamp_to_date_and_time.csv')
+    result_df = timestamp_to_date_and_time(df)
+    expected_df = pd.DataFrame({'created_date': ['1a', '3a'], 'created_time': ['1b', '3b'], 'last_updated_date': ['2a', '4a'], 'last_updated_time': ['2b', '4b']})
+    
+    assert_frame_equal(result_df, expected_df)
+
