6b956fb99ff4794a0f016b95ba249c63349add0b
atif-hussain-khan <atif.hussain.khan@hotmail.com>
2023-08-02 15:49:38 +0100

    added force destroy = true for s3 buckets

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,7 +1,9 @@
 resource "aws_s3_bucket" "ingestion_zone" {
   bucket = "ingestion-va-052023"
+  force_destroy = true
 }
 
 resource "aws_s3_bucket" "processed_zone" {
   bucket = "processed-va-052023"
+  force_destroy = true
 }
