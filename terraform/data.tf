ded6e46446b25cf23009fee9e5ac0d4e7b64d0d2
atif-hussain-khan <atif.hussain.khan@hotmail.com>
2023-08-02 11:27:51 +0100

    different way to archive the lambda

--- terraform/data.tf
+++ terraform/data.tf
@@ -2,33 +2,11 @@ data "aws_caller_identity" "current" {}
 
 data "aws_region" "current" {}
 
-
-locals {
-  source_files = ["${path.module}/../src/extract.py", "${path.module}/../utils"]
-}
-
-data "template_file" "t_file" {
-  count = "${length(local.source_files)}"
-
-  template = "${file(element(local.source_files, count.index))}"
-}
-
-
-data "archive_file" "archive" {
-  type        = "zip"
-  output_path = "${path.module}/../extraction_function.zip"
-
-  source {
-    filename = "${basename(local.source_files[0])}"
-    content  = "${data.template_file.t_file.0.rendered}"
-  }
-
-  source {
-    filename = "${basename(local.source_files[1])}"
-    content  = "${data.template_file.t_file.1.rendered}"
+resource "null_resource" "extraction_zip" {
+  provisioner "local-exec" {
+    command = "zip -r extraction_function.zip /src/extract.py /utils"
   }
 }
-
 
 
 
--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -5,4 +5,5 @@ resource "aws_lambda_function" "extract_lambda" {
   handler = "extract.extraction_lambda_handler"
   runtime = "python3.9"
   layers = [aws_lambda_layer_version.lambda_layer.arn]
+  depends_on = [null_resource.extraction_zip]
 }
