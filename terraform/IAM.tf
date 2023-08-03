ac92c2c72c4baa2319faa2e6fb1c060285c7d911
atif-hussain-khan <atif.hussain.khan@hotmail.com>
2023-08-03 14:34:22 +0100

    fix s3_policy_resource bug

--- terraform/IAM.tf
+++ terraform/IAM.tf
@@ -75,7 +75,7 @@ resource "aws_iam_policy" "s3_policy_resource" {
 
 resource "aws_iam_role_policy_attachment" "lambda_s3_policy_attachment" {
     role = aws_iam_role.lambda_role.name
-    policy_arn = aws_iam_policy.s3_policy.arn
+    policy_arn = aws_iam_policy.s3_policy_resource.arn
 }
 
 
