9123e46a72e2bba81e801a0ce0b5839bc3ec04a1
atif-hussain-khan <atif.hussain.khan@hotmail.com>
2023-08-03 15:22:13 +0100

    create iam infrastructure for extract, load and transform lambda's

--- terraform/IAM.tf
+++ terraform/IAM.tf
@@ -1,7 +1,53 @@
-# IAM Role for all Lambda Functions
-resource "aws_iam_role" "lambda_role" {
-    name = "${local.group_name}_lambda_role"
-    description = "this role is for the extract, transform and load lambda functions"
+#=#=#=#=#=#=#=#=#=#=#=#=# IAM Role - Extract
+resource "aws_iam_role" "extract_lambda_role" {
+    name = "${local.extract_lambda_name}_lambda_role"
+    description = "this role is for the extract lambda"
+    assume_role_policy = <<EOF
+    {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Effect": "Allow",
+                "Action": [
+                    "sts:AssumeRole"
+                ],
+                "Principal": {
+                    "Service": [
+                        "lambda.amazonaws.com"
+                    ]
+                }
+            }
+        ]
+    }
+    EOF
+}
+
+resource "aws_iam_role" "transform_lambda_role" {
+    name = "${local.transform_lambda_name}_lambda_role"
+    description = "this role is for the transform lambda"
+    assume_role_policy = <<EOF
+    {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Effect": "Allow",
+                "Action": [
+                    "sts:AssumeRole"
+                ],
+                "Principal": {
+                    "Service": [
+                        "lambda.amazonaws.com"
+                    ]
+                }
+            }
+        ]
+    }
+    EOF
+}
+
+resource "aws_iam_role" "load_lambda_role" {
+    name = "${local.load_lambda_name}_lambda_role"
+    description = "this role is for the load lambda"
     assume_role_policy = <<EOF
     {
         "Version": "2012-10-17",
@@ -29,7 +75,7 @@ The policy setup for each of the services below follows the following patter:
   aws_iam_role_policy_attachment - attach the policy resource for the service to the lambda role
 */
 
-# SecretsManager Policy
+#=#=#=#=#=#=#=#=#=#=#=#=# SecretsManager Policy
 
 data "aws_iam_policy_document" "sm_document" {
   # Allow the getting of secret values from the specified SecretManager resrouces
@@ -49,39 +95,93 @@ resource "aws_iam_policy" "sm_policy_resource" {
     policy = data.aws_iam_policy_document.sm_document.json
 }
 
-resource "aws_iam_role_policy_attachment" "sm_policy_attachment" {
-    role = aws_iam_role.lambda_role.name
+# Extract
+
+resource "aws_iam_role_policy_attachment" "extract_lambda_sm_policy_attachment" {
+    role = aws_iam_role.extract_lambda_role.name
     policy_arn = aws_iam_policy.sm_policy_resource.arn
 }
 
-# S3 Policy
+# Transform
 
-data "aws_iam_policy_document" "s3_document" {
+resource "aws_iam_role_policy_attachment" "transform_lambda_sm_policy_attachment" {
+    role = aws_iam_role.transform_lambda_name.name
+    policy_arn = aws_iam_policy.sm_policy_resource.arn
+}
+
+# Load
+
+resource "aws_iam_role_policy_attachment" "load_lambda_sm_policy_attachment" {
+    role = aws_iam_role.extract_lambda_role.name
+    policy_arn = aws_iam_policy.load_lambda_name.arn
+}
+
+#=#=#=#=#=#=#=#=#=#=#=#=# S3 Policy
+
+data "aws_iam_policy_document" "s3_ingestion_document" {
   # Allow the uploading and downloading of an object, and listing of buckets from the S3 resources
   statement {
 
     actions = ["s3:PutObject", "s3:GetObject", "s3:ListBucket"]
 
     resources = [
-      "${aws_s3_bucket.ingestion_zone.arn}/*"
+      "${aws_s3_bucket.ingestion_zone.arn}/*",
     ]
   }
 }
 
-resource "aws_iam_policy" "s3_policy_resource" {
+resource "aws_iam_policy" "s3_ingestion_policy_resource" {
     name = "s3_policy"
-    policy = data.aws_iam_policy_document.s3_document.json
+    policy = data.aws_iam_policy_document.s3_ingestion_document.json
 }
 
-resource "aws_iam_role_policy_attachment" "lambda_s3_policy_attachment" {
-    role = aws_iam_role.lambda_role.name
-    policy_arn = aws_iam_policy.s3_policy_resource.arn
+data "aws_iam_policy_document" "s3_processed_document" {
+  # Allow the uploading and downloading of an object, and listing of buckets from the S3 resources
+  statement {
+
+    actions = ["s3:PutObject", "s3:GetObject", "s3:ListBucket"]
+
+    resources = [
+      "${aws_s3_bucket.processed_zone.arn}/*",
+    ]
+  }
 }
 
+resource "aws_iam_policy" "s3_processed_policy_resource" {
+    name = "s3_policy"
+    policy = data.aws_iam_policy_document.s3_processed_document.json
+}
 
-# Cloudwatch Policy
+# Extract
 
-data "aws_iam_policy_document" "cw_document" {
+resource "aws_iam_role_policy_attachment" "extract_lambda_s3_ingestion_policy_attachment" {
+    role = aws_iam_role.extract_lambda_role.name
+    policy_arn = aws_iam_policy.s3_ingestion_policy_resource.arn
+}
+
+# Transform
+
+resource "aws_iam_role_policy_attachment" "transform_lambda_s3_ingestion_policy_attachment" {
+    role = aws_iam_role.transform_lambda_role.name
+    policy_arn = aws_iam_policy.s3_ingestion_policy_resource.arn
+}
+
+resource "aws_iam_role_policy_attachment" "transform_lambda_s3_processed_policy_attachment" {
+    role = aws_iam_role.transform_lambda_role.name
+    policy_arn = aws_iam_policy.s3_processed_policy_resource.arn
+}
+
+# Load
+resource "aws_iam_role_policy_attachment" "load_lambda_s3_processed_policy_attachment" {
+    role = aws_iam_role.load_lambda_role.name
+    policy_arn = aws_iam_policy.s3_processed_policy_resource.arn
+}
+
+#=#=#=#=#=#=#=#=#=#=#=#=# Cloudwatch Policy
+
+# Extract
+
+data "aws_iam_policy_document" "extract_lambda_cw_document" {
   # Allow the creation of a log group inside the specified account
   statement {
 
@@ -103,12 +203,80 @@ data "aws_iam_policy_document" "cw_document" {
   }
 }
 
-resource "aws_iam_policy" "cw_policy_resource" {
-    name = "cw_policy"
+resource "aws_iam_policy" "extract_lambda_cw_policy_resource" {
+    name = "${local.extract_lambda_name}_cw_policy"
     policy = data.aws_iam_policy_document.cw_document.json
 }
 
-resource "aws_iam_role_policy_attachment" "lambda_cw_policy_attachment" {
-    role = aws_iam_role.lambda_role.name
-    policy_arn = aws_iam_policy.cw_policy_resource.arn
+resource "aws_iam_role_policy_attachment" "extract_lambda_cw_policy_attachment" {
+    role = aws_iam_role.extract_lambda_role.name
+    policy_arn = aws_iam_policy.extract_lambda_cw_policy_resource.arn
+}
+
+# Transform
+
+data "aws_iam_policy_document" "transform_lambda_cw_document" {
+  # Allow the creation of a log group inside the specified account
+  statement {
+
+    actions = [ "logs:CreateLogGroup" ]
+
+    resources = [
+      "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:*"
+    ]
+  }
+  
+  # Allow the creation of a log stream and put log events in the specified log group  
+  statement {
+
+    actions = [ "logs:CreateLogStream", "logs:PutLogEvents" ]
+
+    resources = [
+      "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:/aws/lambda/${local.transform_lambda_name}:*"
+    ]
+  }
+}
+
+resource "aws_iam_policy" "transform_lambda_cw_policy_resource" {
+    name = "${local.transform_lambda_name}_cw_policy"
+    policy = data.aws_iam_policy_document.transform_lambda_cw_document.json
+}
+
+resource "aws_iam_role_policy_attachment" "transform_lambda_cw_policy_attachment" {
+    role = aws_iam_role.transform_lambda_role.name
+    policy_arn = aws_iam_policy.transform_lambda_cw_policy_resource.arn
+}
+
+# Load
+
+data "aws_iam_policy_document" "load_lambda_cw_document" {
+  # Allow the creation of a log group inside the specified account
+  statement {
+
+    actions = [ "logs:CreateLogGroup" ]
+
+    resources = [
+      "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:*"
+    ]
+  }
+  
+  # Allow the creation of a log stream and put log events in the specified log group  
+  statement {
+
+    actions = [ "logs:CreateLogStream", "logs:PutLogEvents" ]
+
+    resources = [
+      "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:/aws/lambda/${local.load_lambda_name}:*"
+    ]
+  }
+}
+
+resource "aws_iam_policy" "load_lambda_cw_policy_resource" {
+    name = "${local.load_lambda_name}_cw_policy"
+    policy = data.aws_iam_policy_document.load_lambda_cw_document.json
+}
+
+resource "aws_iam_role_policy_attachment" "load_lambda_cw_policy_attachment" {
+    role = aws_iam_role.load_lambda_role.name
+    policy_arn = aws_iam_policy.load_lambda_cw_policy_resource.arn
 }
