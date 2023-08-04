965ad41608a8cb8b306b2856f17780d0c74dc677
atif-hussain-khan <atif.hussain.khan@hotmail.com>
2023-08-04 09:42:05 +0100

    complete infrastructure setup less triggers

--- terraform/data.tf
+++ terraform/data.tf
@@ -3,8 +3,17 @@ data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 locals {
   temp_extract_lambda_dir= "${path.root}/../temp_${local.extract_lambda_name}"
-  extract_function_zip_path = "${path.module}/../extraction_function.zip"
+  temp_transform_lambda_dir= "${path.root}/../temp_${local.transform_lambda_name}"
+  temp_load_lambda_dir= "${path.root}/../temp_${local.load_lambda_name}"
+  
   extract_function_path = "${path.root}/../src/extract.py"
+  transform_function_path = "${path.root}/../src/transform.py"
+  load_function_path = "${path.root}/../src/load.py"
+
+  extract_function_zip_path = "${path.module}/../extraction_function.zip"
+  transform_function_zip_path = "${path.module}/../transform_function.zip"
+  load_function_zip_path = "${path.module}/../load_function.zip"
+  
   utils_path = "${path.root}/../utils"
 }
 
@@ -15,6 +24,28 @@ resource "null_resource" "extraction_zip" {
       cp -r ${local.extract_function_path} ${local.utils_path} ${local.temp_extract_lambda_dir}
       cd ${local.temp_extract_lambda_dir} 
       zip -r ${local.extract_function_zip_path} .
+    EOT
+  }
+}
+
+resource "null_resource" "transform_zip" {
+  provisioner "local-exec" {
+    command = <<EOT
+      mkdir ${local.temp_transform_lambda_dir}
+      cp -r ${local.transform_function_path} ${local.utils_path} ${local.temp_transform_lambda_dir}
+      cd ${local.temp_transform_lambda_dir} 
+      zip -r ${local.transform_function_zip_path} .
+    EOT
+  }
+}
+
+resource "null_resource" "load_zip" {
+  provisioner "local-exec" {
+    command = <<EOT
+      mkdir ${local.temp_load_lambda_dir}
+      cp -r ${local.load_function_path} ${local.utils_path} ${local.temp_load_lambda_dir}
+      cd ${local.temp_load_lambda_dir} 
+      zip -r ${local.load_function_zip_path} .
     EOT
   }
 }
--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -8,3 +8,25 @@ resource "aws_lambda_function" "extract_lambda" {
   timeout = 60
   depends_on = [null_resource.extraction_zip]
 }
+
+resource "aws_lambda_function" "transform_lambda" {
+  filename = "${local.transform_function_zip_path}"
+  function_name = "${local.transform_lambda_name}"
+  role = aws_iam_role.transform_lambda_role.arn
+  handler = "transform.transform_lambda_handler"
+  runtime = "python3.9"
+  layers = [aws_lambda_layer_version.lambda_layer.arn, "arn:aws:lambda:eu-west-2:336392948345:layer:AWSSDKPandas-Python39:8"]
+  timeout = 60
+  depends_on = [null_resource.transform_zip]
+}
+
+resource "aws_lambda_function" "load_lambda" {
+  filename = "${local.load_function_zip_path}"
+  function_name = "${local.load_lambda_name}"
+  role = aws_iam_role.load_lambda_role.arn
+  handler = "load.load_lambda_handler"
+  runtime = "python3.9"
+  layers = [aws_lambda_layer_version.lambda_layer.arn, "arn:aws:lambda:eu-west-2:336392948345:layer:AWSSDKPandas-Python39:8"]
+  timeout = 60
+  depends_on = [null_resource.load_zip]
+}
